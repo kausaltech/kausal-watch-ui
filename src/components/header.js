3c9e2eebf4c754abfdb1b95d63508aa91803d927
Tero Tikkanen <tero.tikkanen@gmail.com>
2018-11-26 23:29:42 +0200

    Recolor navbar

--- src/components/header.js
+++ src/components/header.js
@@ -2,26 +2,30 @@ import React from 'react'
 import { Link } from "gatsby";
 import { Navbar } from 'reactstrap';
 
-import helLogo from 'open-city-design/src/assets/helsinki-logo-white.svg';
-
 import styled from 'styled-components';
 
 const TopNav = styled(Navbar)`
   background-color: ${props => props.theme.helTram};
 `
 
-const Logo = styled.img`
+const BotNav = styled(Navbar)`
+  background-color: ${props => props.theme.white};
+  color: ${props => props.theme.helTram};
+`
+
+const Logo = styled.div`
+  width: 5em;
   height: 2em;
 `
 
 const Header = ({ siteTitle }) => (
   <div>
     <TopNav expand="md">
-      <Link to="/" aria-label="Helsinki, palvelun etusivu" className="navbar-brand"><Logo src={`${helLogo}`} aria-hidden="true" /></Link>
+      <Link to="/" aria-label="Helsinki, palvelun etusivu" className="navbar-brand"><Logo aria-hidden="true" className="hel-logo-summer"/></Link>
     </TopNav>
-    <Navbar color="light" light expand="md">
+    <BotNav expand="md">
       <Link to="/" className="navbar-brand">Hiilineutraali Helsinki</Link>
-    </Navbar>
+    </BotNav>
   </div>
 )
 
