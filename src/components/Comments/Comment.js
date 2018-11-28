dab49d36c0465574fe519f4f7134499021b7cb4a
Tero Tikkanen <tero.tikkanen@gmail.com>
2018-11-28 11:45:52 +0200

    Get comments from kerrokantasi API

--- src/components/Comments/Comment.js
+++ src/components/Comments/Comment.js
@@ -1,28 +1,47 @@
 import React from 'react';
+import moment from 'moment';
 import { Button } from 'reactstrap';
 
 class Comment extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.showAuthor = this.showAuthor.bind(this);
+    this.parseTimestamp = this.parseTimestamp.bind(this);
+  }
   
+  showAuthor(author) {
+    if (author != null) return (<span>{author}</span>)
+    else return (<span>Anonymous</span>)
+  }
+  
+  parseTimestamp = (timestamp) => {
+    const timeFormat = 'DD.MM.YYYY hh:mm';
+    return moment(timestamp).format(timeFormat);
+  }
+    
   render() {
+    const comment = this.props.comment;
+    
     return (
       <div className="hearing-comment">
         <div className="hearing-comment-header clearfix">
           <div className="hearing-comment-votes">
             <Button className="btn-sm hearing-comment-vote-link">
-              <span name="thumbs-o-up"/> 4
+              <span name="thumbs-o-up"/> {comment.n_votes}
             </Button>
           </div>
           <div className="hearing-comment-publisher">
             <span className="hearing-comment-user">
-              Anonymous
+              {this.showAuthor(comment.author_name)}
             </span>
             <span className="hearing-comment-date">
-              29.11.2018
+              {this.parseTimestamp(comment.created_at)}
             </span>
           </div>
         </div>
         <div className="hearing-comment-body">
-          <p>{this.props.content}</p>
+          <p>{comment.content}</p>
         </div>
       </div>
     );
--- src/components/Comments/CommentList.js
+++ src/components/Comments/CommentList.js
@@ -1,34 +1,54 @@
 import React from 'react';
 
 import Comment from './Comment';
+import ContentLoader from '../ContentLoader';
 
 class CommentList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      comments: [],
+      commentCount: [],
+    };
+  }
 
+  componentDidMount() {
+    fetch(process.env.GATSBY_KK_API + "/comment/?authorization_code=&section=5rIcblAyv2TDP881lLeUiruj1DSllMUh&hearing=")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log('comments loaded');
+          this.setState({
+            isLoaded: true,
+            comments: result.results,
+            commentCount: result.count
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+  
   render() {
-    return (
+    if (this.error) {
+      return <div>Error: {this.state.error.message}</div>;
+    } else if (!this.state.isLoaded) {
+      return <ContentLoader />;
+    } else {
+      return (
       <div className="comment-list">
-        <div className="comment-form-container">
-            <div className="comment-form"><form>
-              <h4><span>Kommentti</span></h4>
-              
-              <textarea className="form-control">Kommentointi ei vielä toimi. Tämä komponentti näyttää kommentoinnin paikan.</textarea>
-              
-              <h4><span>Nimimerkki</span></h4>
-              <div className="form-group"><input type="text" placeholder="Anonyymi" defaultValue="" maxLength="32" className="form-control" /></div>
-              <div className="comment-buttons clearfix"><button type="button" className="btn btn-default"><span>Peru</span></button><button disabled="" type="button" className="btn btn-primary"><span>Lähetä</span></button></div>
-              <div className="comment-conditions"><span>Annetut kommentit julkaistaan <a href="http://www.hri.fi/fi/mita-on-avoin-data/"><span>avoimena datana</span> </a>Helsingin kaupungin palvelussa <a href="http://creativecommons.org/licenses/by/4.0/deed.fi"><span>avoimella lisenssillä</span></a>. Voit jättää kommenttisi nimettömänä, jos et ole kirjautunut sisään. Lisäksi tekstejä tarvittaessa muokataan loukkaavan tai muutoin törkeän kielenkäytön osalta.</span></div>
-            </form></div>
-        </div>
-        <Comment content="Tämä on testikommentti. Eikö olekin hieno?" />
-        <Comment content="On niitä parempiakin kommentteja nähty." />
-        <Comment content="Jotkut kommentoijat eivät osaa tiivistää ja kirjoittavat ummet ja lammet. On ihan tärkeää perustella mielipiteensä, mutta etenkin toisten keskustelijoiden väsyttäminen turhalla jaarittelulla on tylsää. Lisäksi liian pitkät kommenttitekstit vievät tilaa ja ottavat koko keskustelun helposti haltuunsa." />
-        <Comment content="Eikä!" />
-        <Comment content="Kommentointiin käytämme Helsingin kaupungin omaa Kerrokantasi rajapintaa." />
-        <Comment content="Kerrokantasi on kyllä ihan huippu."/>
-        <Comment content="Tälläkö sitä ilmastonmuutos nyt pysäytetään?" />
+         {this.state.comments.map(item => (
+            <Comment key={item.id} comment={item} />
+          ))}
       </div>
     );
   }
 }
-
+}
 export default CommentList;
