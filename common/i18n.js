4469dddc8f74ee2250e3f366c701a860f6a29153
Juha Yrjölä <juha.yrjola@iki.fi>
2019-09-20 15:56:07 +0300

    Disable language detection

--- common/i18n.js
+++ common/i18n.js
@@ -4,6 +4,8 @@ const NextI18Next = require('next-i18next').default;
 module.exports = new NextI18Next({
   defaultLanguage: 'fi',
   otherLanguages: ['en'],
+  browserLanguageDetection: false,
+  serverLanguageDetection: false,
   localeSubpaths: {
     en: 'en',
   },
