b2d63bcd3b7042d4b1881ac4831eb03765dd9822
Tero Tikkanen <tero.tikkanen@gmail.com>
2019-10-22 22:16:05 -0400

    Add favicons

--- components/layout.js
+++ components/layout.js
@@ -28,6 +28,13 @@ function Layout({ children }) {
             <meta property="og:url" content={plan.currentURL.domain + plan.currentURL.path} />
           }
           <meta property="og:site_name" content={siteTitle} />
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png"/ >
+          <link rel="manifest" href="/static/favicon/site.webmanifest" />
+          <link rel="mask-icon" href="/static/favicon/safari-pinned-tab.svg" color="#000000" />
+          <meta name="msapplication-TileColor" content="#ffc40d" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <Header siteTitle={siteTitle} />
         {children}
