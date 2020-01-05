924e76feda79b8b34973ec6cc7f5d7da0b5d12c1
Juha Yrjölä <juha.yrjola@iki.fi>
2020-01-05 14:44:07 +0200

    Fix a bug when Person.organization is null

--- components/actions/ContactPerson.js
+++ components/actions/ContactPerson.js
@@ -89,10 +89,16 @@ const ContactDetails = (props) => {
         if (error) return <span>{error.message}</span>;
         const { person } = data;
 
-        const orgAncestors = person.organization.ancestors
-          .filter((org) => org.classification.name !== 'Valtuusto' && org.classification.name !== 'Hallitus')
-          .map((org) => ({ id: org.id, name: org.name }));
-        orgAncestors.push({ id: person.organization.id, name: person.organization.name });
+        let orgAncestors;
+
+        if (person.organization && person.organization.ancestors) {
+          orgAncestors = person.organization.ancestors
+            .filter((org) => org.classification.name !== 'Valtuusto' && org.classification.name !== 'Hallitus')
+            .map((org) => ({ id: org.id, name: org.name }));
+          orgAncestors.push({ id: person.organization.id, name: person.organization.name });
+        } else {
+          orgAncestors = [];
+        }
 
         return (
           <div className="mt-2">
