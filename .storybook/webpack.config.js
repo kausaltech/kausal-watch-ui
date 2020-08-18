88f89e937e9061979a40963b58fbdee24f6340ba
Tero Tikkanen <tero.tikkanen@gmail.com>
2020-08-18 18:11:22 -0400

    Storybook resolve absolute module paths

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,7 +1,13 @@
+const path = require('path');
+
 module.exports = ({ config }) => {
     config.plugins = config.plugins.filter(
         p => String(p.resourceRegExp) !== '/core-js/'
       );
-  
+    config.resolve.modules = [
+      ...(config.resolve.modules || []),
+      path.resolve('./'),
+    ];
     return config;
   };
+  
