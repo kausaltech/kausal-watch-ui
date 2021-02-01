6ddf19d6b39d9c71645f8d8ad58ca2d5c3aa07d8
Juha Yrjölä <juha.yrjola@iki.fi>
2021-02-01 17:36:51 +0200

    Convert static page component to use new GraphQL API

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -9,8 +9,9 @@ import { useTranslation } from 'common/i18n';
 import Layout, { Meta } from 'components/layout';
 import PlanContext from 'context/plan';
 import ErrorMessage from 'components/common/ErrorMessage';
-import Accordion from 'components/common/Accordion';
 import ContentLoader from 'components/common/ContentLoader';
+import StreamField from 'components/common/StreamField';
+
 
 const HeaderImage = styled.div`
   background-image: url(${(props) => props.image});
@@ -38,68 +39,51 @@ const ContentHeader = styled.header`
   }
 `;
 
-const ContentMarkup = styled.div`
-  padding: ${(props) => props.theme.spaces.s300} 0;
-`;
-
-const FaqSection = styled.section`
-  padding: ${(props) => props.theme.spaces.s400} 0;
-  background: ${(props) => props.theme.themeColors.light};
-
-  h2 {
-    text-align: center;
-    font-size: ${(props) => props.theme.fontSizeXl};
-    margin-bottom: ${(props) => props.theme.spaces.s300};
-  }
-`;
-
-const GET_CONTENT = gql`
-query GetStaticPage($plan: ID!, $slug: ID!) {
-  staticPage(plan: $plan, slug: $slug) {
+const GET_PLAN_PAGE = gql`
+query GetPlanPage($plan: ID!, $path: String!) {
+  planPage(plan: $plan, path: $path) {
+    __typename
     id
     slug
-    name
     title
-    tagline
-    content
-    modifiedAt
-    imageUrl(size: "1800x1200")
-    questions {
-      id
-      title
-      answer
+    ... on StaticPage {
+      headerImage {
+        rendition(size: "1200x800") {
+          src
+        }
+      }
+      leadParagraph
+      body {
+        ...StreamFieldFragment
+      }
     }
+    lastPublishedAt
   }
-}`;
+}
+${StreamField.fragments.streamField}
+`;
 
 function StaticPage({ slug }) {
   const { t } = useTranslation();
-
-  // We don't support nested static pages yet
-  if (slug.length > 1) {
-    return <ErrorMessage statusCode={404} message={t('page-not-found')} />;
-  }
-  slug = slug[0];
-
+  const path = '/' + slug.join('/');
   const plan = useContext(PlanContext);
-  const { loading, error, data } = useQuery(GET_CONTENT, {
+  const { loading, error, data } = useQuery(GET_PLAN_PAGE, {
     variables: {
       plan: plan.identifier,
-      slug,
+      path,
     },
   });
-
   if (loading) return <ContentLoader />;
   if (error) return <ErrorMessage message={error.message} />;
 
-  const { staticPage } = data;
-  if (!staticPage) {
+  const { planPage } = data;
+  if (!planPage) {
     return <ErrorMessage statusCode={404} message={t('page-not-found')} />;
   }
-
+  console.log(planPage);
   return (
     <Layout>
-      <Content page={data.staticPage} />
+      <Content page={planPage} />
     </Layout>
   );
 }
@@ -109,7 +93,8 @@ StaticPage.getInitialProps = async ({ query }) => ({
 });
 
 const Content = ({ page }) => {
-  const { title, tagline, imageUrl, content, questions } = page;
+  const { title, headerImage } = page;
+  const imageUrl = headerImage?.rendition.url;
   const { t } = useTranslation(['common']);
 
   return (
@@ -117,7 +102,7 @@ const Content = ({ page }) => {
       <Meta
         title={`${title}`}
         shareImageUrl={imageUrl}
-        description={`${tagline}`}
+        description={`${page.searchDescription || title}`}
         />
       <HeaderBg>
         { imageUrl && (
@@ -137,44 +122,14 @@ const Content = ({ page }) => {
             <Col>
               <ContentHeader>
                 <h1>{title}</h1>
-                <p className="lead">{tagline}</p>
+                {page.leadParagraph && <p className="lead">{page.leadParagraph}</p>}
               </ContentHeader>
             </Col>
           </Row>
         </Container>
       </HeaderBg>
       <div className="content-area">
-        <Container>
-          <Row>
-            <Col lg={{ size:8, offset: 2 }} md={{ size: 10, offset: 1 }}>
-              <ContentMarkup dangerouslySetInnerHTML={{ __html: content }} />
-            </Col>
-          </Row>
-        </Container>
-        { questions.length > 0 &&
-          (
-          <FaqSection>
-            <Container>
-              <Row>
-                <Col lg={{ size: 8, offset: 2 }} md={{ size: 10, offset: 1 }}>
-                  <h2>{ t('frequently-asked-questions') }</h2>
-                  <Accordion>
-                    { questions.map(faq => (
-                      <Accordion.Item key={faq.id} id={faq.id}>
-                        <Accordion.Header>
-                          {faq.title}
-                        </Accordion.Header>
-                        <Accordion.Body>
-                          <div className="text-content" dangerouslySetInnerHTML={{ __html: faq.answer }}/>
-                        </Accordion.Body>
-                      </Accordion.Item>
-                    ))}
-                  </Accordion>
-                </Col>
-              </Row>
-            </Container>
-          </FaqSection>
-          )}
+        {page.body && <StreamField blocks={page.body} />}
       </div>
     </article>
   );
