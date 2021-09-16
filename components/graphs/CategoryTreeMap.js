1b93d1f354fec4e33d6d00a86ba8285423e3e191
Juha Yrjölä <juha.yrjola@iki.fi>
2021-09-16 17:48:19 +0300

    Clean up CategoryTreeMap, add intl number formatting

--- components/graphs/CategoryTreeMap.js
+++ components/graphs/CategoryTreeMap.js
@@ -1,40 +1,61 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 import _ from 'lodash';
+import { useTranslation } from 'common/i18n';
 
-function makeTrace(cats) {
-  const hasChildren = new Map();
-  cats.forEach((cat) => {
-    if (cat.parent) hasChildren.set(cat.parent.id, true);
+function makeTrace(catsIn, i18n) {
+  const { language } = i18n;
+  const numberFormat = new Intl.NumberFormat(language, {
+    maximumSignificantDigits: 3,
   });
+  const cats = catsIn.map((cat) => ({ ...cat, value: 0, children: [] }));
 
-  const rootSection = {
+  // Include the top-level sector
+  cats.push({
     id: 'root',
     name: 'Total utsläpp',
     color: '#999999',
     parent: null,
-    metadata: [
-      {
-        value: 50.921,
-      },
-    ],
-  };
+    value: 0,
+    children: [],
+  });
+
+  const catMap = new Map(cats.map((cat) => [cat.id, cat]));
+  cats.forEach((cat) => {
+    if (!cat.parent) {
+      if (cat.id !== 'root') {
+        // Bind the previous top-level sectors to root
+        cat.parent = { id: 'root' };
+      }
+    }
+    if (cat.parent) {
+      const parent = catMap.get(cat.parent.id);
+      parent.children.push(cat);
+    }
+  });
+  // Aggregate the treemap values starting from the leaves
+  cats.filter((cat) => cat.children.length === 0).forEach((cat) => {
+    let { parent } = cat;
+    cat.value = cat.metadata[0].value;
+    while (parent) {
+      const p = catMap.get(parent.id);
+      p.value += cat.value;
+      parent = p.parent;
+    }
+  });
 
   const trace = {
     type: 'icicle',
     name: 'Utsläpp',
-    labels: _.concat(`<b>${rootSection.name}</b>`, cats.map((cat) => `<b>${cat.name}</b>`)),
-    text: _.concat(`${rootSection.metadata[0]?.value} CO<sub>2</sub>e`, cats.map((cat) => (cat.metadata[0]?.value ? `${cat.metadata[0]?.value} Mt` : '-'))),
-    ids: _.concat('root', cats.map((cat) => cat.id)),
-    parents: _.concat(null, cats.map((cat) => cat.parent?.id || 'root')),
-    values: _.concat(0, cats.map((cat) => {
-      if (hasChildren.get(cat.id)) return 0;
-      return cat.metadata[0]?.value || 0;
-    })),
+    labels: cats.map((cat) => `<b>${cat.name}</b>`),
+    text: cats.map((cat) => `${numberFormat.format(cat.value)} Mt`),
+    ids: cats.map((cat) => cat.id),
+    parents: cats.map((cat) => cat.parent?.id),
+    values: cats.map((cat) => cat.value),
     marker: {
-      colors: _.concat('#B4B4B4', cats.map((cat) => cat.color || null)),
+      colors: cats.map((cat) => cat.color),
     },
-    branchvalues: 'remainder',
+    branchvalues: 'total',
     maxdepth: 2,
     textinfo: 'label+text',
     pathbar: {
@@ -51,13 +72,14 @@ function makeTrace(cats) {
 
 const CategoryTreeMap = React.memo((props) => {
   const { data, onChangeSection } = props;
+  const { i18n } = useTranslation();
 
   if (!process.browser) {
     return null;
   }
 
   const Plot = dynamic(import('./Plot'));
-  const trace = makeTrace(data);
+  const trace = makeTrace(data, i18n);
 
   // console.log(trace);
   const layout = {
