24eed74818172738416fc0dc370e6485d527eb81
Tero Tikkanen <tero.tikkanen@gmail.com>
2021-09-22 19:15:35 +0200

    Show RelatedIndicators side by side

--- components/contentblocks/RelatedIndicatorsBlock.js
+++ components/contentblocks/RelatedIndicatorsBlock.js
@@ -2,8 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Container, Row, Col } from 'reactstrap';
 import styled from 'styled-components';
-import IndicatorHighlightCard from 'components/indicators/IndicatorHighlightCard';
+import { Link } from 'routes';
 import IndicatorGraph from 'components/graphs/IndicatorGraph';
+import { useTranslation } from 'common/i18n';
+import Icon from 'components/common/Icon';
 
 const IndicatorGraphSection = styled.div`
   background-color: ${(props) => props.theme.neutralLight};
@@ -11,13 +13,27 @@ const IndicatorGraphSection = styled.div`
 `;
 
 const IndicatorItem = (props) => {
-  const { indicator } = props;
+  const { indicator, indicatorCount } = props;
+  const { t } = useTranslation();
+  const singleIndicator = indicatorCount === 1;
+  const showLink = false;
+
   return (
     <Col
       className="mb-5"
-      lg={{ size: 8, offset: 2 }}
+      lg={{ size: singleIndicator ? 8 : 6, offset: singleIndicator ? 2 : 0 }}
     >
       <IndicatorGraph indicatorId={indicator} />
+      { showLink && (
+      <div className="mt-2 text-right">
+        <Link href={`/indicators/${indicator}`}>
+          <a href>
+            {t('read-more')}
+            <Icon name="arrowRight" />
+          </a>
+        </Link>
+      </div>
+      )}
     </Col>
   );
 };
@@ -27,11 +43,12 @@ const RelatedIndicatorsblock = (props) => {
   return (
     <IndicatorGraphSection>
       <Container>
-        <Row>
+        <Row className="align-items-end">
           { indicators?.map((item) => (
             <IndicatorItem
               indicator={item.id}
               key={item.id}
+              indicatorCount={indicators.length}
             />
           ))}
         </Row>
