da9b89e981b443938dbb6bc77ea46def20ce0ede
Tero Tikkanen <tero.tikkanen@gmail.com>
2022-03-16 11:42:39 +0200

    Use Popper for navbarsearch results

--- components/common/NavbarSearch.js
+++ components/common/NavbarSearch.js
@@ -1,6 +1,7 @@
 import { useState, useRef } from 'react';
 import { useRouter } from 'next/router';
-import { InputGroup } from 'reactstrap';
+import { InputGroup, Popover, PopoverBody } from 'reactstrap';
+import { usePopper } from 'react-popper';
 import styled from 'styled-components';
 import { SearchProvider, WithSearch, SearchBox, Results } from '@elastic/react-search-ui';
 
@@ -8,12 +9,12 @@ import { useTheme } from 'common/theme';
 import WatchSearchAPIConnector from 'common/search';
 import Button from './Button';
 import Icon from './Icon';
+import PlanTag from 'components/plans/PlanTag';
 import { usePlan } from 'context/plan';
 import { useApolloClient } from '@apollo/client';
 import { useTranslation } from 'common/i18n';
 
 const TextInput = styled.input`
-  width: ${(props) => (props.isOpen === 'true' ? 'auto' : '0')};
   height: calc(${(props) => props.theme.inputLineHeight}em + ${(props) => props.theme.inputPaddingY} + ${(props) => props.theme.inputPaddingY});
   padding: ${(props) => (props.isOpen === 'true' ? `${props.theme.inputPaddingY} ${props.theme.inputPaddingY}` : '0')};
   color: ${(props) => props.theme.brandNavColor};
@@ -26,38 +27,91 @@ const TextInput = styled.input`
     padding: ${(props) => props.theme.inputPaddingY} ${(props) => props.theme.inputPaddingX};
     border-width: ${(props) => props.theme.inputBorderWidth};
   }
+`;
 
+const ResultsBox = styled.div`
+  display: inline-block;
+  z-index: 2000;
+  width: 400px;
+  padding: 5px 10px;
+  font-size: 13px;
+  border-radius: 6px;
+  background: #FFFFFF;
+  box-shadow: 2px 2px 8px 1px rgba(0, 0, 0, .5);
+`;
+
+const HitList = styled.ul`
+  list-style: none;
+`;
+
+const HitItem = styled.li`
+
+`;
+
+const SearchControls = styled.div`
+
+.sui-search-box__text-input {
+
+}
+
+.sui-search-box__submit {
+  display: none;
+}
+`;
+
+
+const Arrow = styled.div`
+  position: absolute;
+  width: 8px;
+  height: 8px;
+  background: inherit;
+  top: -4px;
+  visibility: hidden;
+
+  &::before {
+    position: absolute;
+    width: 8px;
+    height: 8px;
+    background: inherit;
+  }
+
+  &::before {
+    visibility: visible;
+    content: '';
+    transform: rotate(45deg);
+  }
 `;
 
 function ResultItem({ hit }) {
   const itemImage = hit.plan.image?.rendition?.src;
   // FIXME: Group by hit.object.__typename?
   return (
-    <li key={hit.id}>
+    <HitItem key={hit.id}>
       <a href={hit.url}>
-        {itemImage && (
-          <img src={itemImage} />
-        )}
-        <div>{ hit.plan.shortName || hit.plan.name }</div>
+        <PlanTag
+          planImage={itemImage}
+          planShortName={hit.plan.shortName || hit.plan.name}
+        />
         <div>{ hit.title }</div>
         {hit.highlight && (
           <div dangerouslySetInnerHTML={{__html: hit.highlight}} />
         )}
       </a>
-    </li>
+    </HitItem>
   )
 }
 
 function ResultList({ results, searchTerm }) {
-  const plan = usePlan();
+  //const plan = usePlan();
+  const [referenceElement, setReferenceElement] = useState(null);
   if (!results) return null;
   // FIXME: Add 'Search for {searchTerm}' result
   return (
-    <ul style={{backgroundColor: "#ffffff"}}>
-      {results.map(r => (
-        <ResultItem hit={r} />
-      ))}
-    </ul>
+        <HitList>
+          {results.map(r => (
+            <ResultItem hit={r} />
+          ))}
+        </HitList>
   );
 }
 
@@ -67,18 +121,18 @@ function NavbarSearch(props) {
   const plan = usePlan();
   const { t } = useTranslation();
   const apolloClient = useApolloClient();
-  const [searchOpen, setSearchOpen] = useState(false);
   const router = useRouter();
-
-  const handleFocus = (e) => {
-    if (!e.currentTarget.contains(e.relatedTarget)) {
-      setSearchOpen(false);
-    }
-  };
 
   const connector = new WatchSearchAPIConnector({ plan, apolloClient });
   return (
-    <SearchProvider config={{ apiConnector: connector }} >
+    <SearchProvider config={{
+      apiConnector: connector,
+      hasA11yNotifications: true,
+      a11yNotificationMessages: {
+        searchResults: ({ start, end, totalResults, searchTerm }) =>
+          `Searching for "${searchTerm}". Showing ${start} to ${end} results out of ${totalResults}.`
+      },
+    }} >
       <WithSearch
         mapContextToProps={({ searchTerm, setSearchTerm, results }) => ({
           searchTerm,
@@ -88,52 +142,53 @@ function NavbarSearch(props) {
       >
         {(context) => {
           const { searchTerm, setSearchTerm, results } = context;
-          const handleSubmit = (event) => {
-            event.preventDefault();
-            if (!searchOpen) {
-              searchInput.current.focus();
-              setSearchOpen(true);
-            } else if (searchInput.current.value) {
-              router.push({
-                pathname: '/search',
-                query: { q: searchTerm },
-              });
-              setSearchOpen(false);
-            } else setSearchOpen(false);
-          };
+          const [referenceElement, setReferenceElement] = useState(null);
+          const [popperElement, setPopperElement] = useState(null);
+          const [arrowElement, setArrowElement] = useState(null);
+          const { styles, attributes, update } = usePopper(referenceElement, popperElement, {
+            modifiers: [
+              { name: 'arrow', options: { element: arrowElement } },
+              {
+                name: 'offset',
+                options: {
+                  offset: [0, 10],
+                },
+              },],
+          });
+
           return (
             <div>
-              <form autoComplete="off">
-                <InputGroup onBlur={handleFocus}>
-                  <TextInput
-                    type="search"
-                    id="q"
-                    name="q"
-                    autocomplete="false"
-                    placeholder={t('search')}
-                    aria-label={t('search')}
-                    value={searchTerm}
-                    onChange={e => setSearchTerm(e.target.value, {
-                      autocompleteResults: true,
-                      autocompleteMinimumCharacters: 2,
-                      debounce: 200,
-                    })}
-                    ref={searchInput}
-                    isOpen={searchOpen.toString()}
-                  />
-                  <Button
-                    color={!searchOpen ? 'link' : 'primary'}
-                    type="submit"
-                    onClick={handleSubmit}
-                  >
-                    <Icon
-                      name="search"
-                      color={theme.themeColors.white}
-                    />
-                  </Button>
-                </InputGroup>
-              </form>
-              <ResultList results={results} searchTerm={searchTerm} />
+              <SearchControls ref={setReferenceElement}>
+              <SearchBox
+                autocompleteResults={{
+                  titleField: "title",
+                  urlField: "nps_link"
+                }}
+                onSubmit={searchTerm => {
+                  router.push({
+                    pathname: '/search',
+                    query: { q: searchTerm },
+                  });
+                }}
+                onSelectAutocomplete={(selection, {}, defaultOnSelectAutocomplete) => {
+                  if (selection.suggestion) {
+                    router.push({
+                      pathname: '/search',
+                      query: { q: selection.suggestion },
+                    });
+                  } else {
+                    defaultOnSelectAutocomplete(selection);
+                  }
+                }}
+              />
+              </SearchControls>
+              { results.length > 0 &&
+                <ResultsBox ref={setPopperElement} style={styles.popper} {...attributes.popper}>
+                  <Arrow ref={setArrowElement} style={styles.arrow} />
+                  <ResultList results={results} searchTerm={searchTerm} anchor={referenceElement}/>
+                </ResultsBox>
+              }
+
             </div>
           )
         }}
