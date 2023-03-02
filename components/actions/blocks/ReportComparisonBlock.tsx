9a68e22a90ee188b3bd75a909b21aa518f6f68a5
Tero Tikkanen <tero.tikkanen@gmail.com>
2023-03-02 16:57:57 +0200

    Make reportcomparison collapsed

--- components/actions/blocks/ReportComparisonBlock.tsx
+++ components/actions/blocks/ReportComparisonBlock.tsx
@@ -1,10 +1,31 @@
+import React, { useState } from 'react';
 import dayjs from 'common/dayjs';
 
 import styled from 'styled-components';
-import { Row, Col } from 'reactstrap';
-import { ActionSection, SectionHeader } from 'components/actions/ActionContent';
+import { Row, Col, Button, Collapse } from 'reactstrap';
 import { useTranslation } from 'common/i18n';
 import ActionAttribute from 'components/common/ActionAttribute';
+import Icon from 'components/common/Icon';
+
+
+const ReportSection = styled.div`
+  margin-bottom: ${(props) => props.theme.spaces.s200};
+  padding-bottom: ${(props) => props.theme.spaces.s050};
+  border-top: 1px solid ${(props) => props.theme.graphColors.grey020};
+  border-bottom: 1px solid ${(props) => props.theme.graphColors.grey020};
+`;
+
+const SectionHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  margin: ${(props) => props.theme.spaces.s050} 0;
+
+  h2 {
+    font-size: ${(props) => props.theme.fontSizeLg};
+    margin: 0;
+  }
+`;
 
 const FieldHeader = styled.div`
   font-size: ${(props) => props.theme.fontSizeSm};
@@ -14,10 +35,27 @@ const FieldHeader = styled.div`
 const ReportName = styled.div`
 `;
 
+const ToggleButton = styled(Button)`
+  padding: 0;
+  margin: 0;
+  color: ${(props) => props.theme.themeColors.dark};
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  &.open {
+    color: ${(props) => props.theme.graphColors.grey050};
+  }
+`;
+
 const ReportFieldComparison = styled.div`
   display: flex;
   overflow-x: auto;
-  padding: ${(props) => props.theme.spaces.s100} 0;
+  padding: ${(props) => props.theme.spaces.s050} 0;
+  margin-bottom: ${(props) => props.theme.spaces.s050};
+
   background-image: 
     linear-gradient(to right, white, white),
     linear-gradient(to right, white, white),
@@ -45,6 +83,9 @@ const ReportDate = styled.span`
 const ReportComparisonBlock = (props) => {
   const { block, action } = props;
   const { t } = useTranslation();
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
   const { reportField, reportsToCompare } = block;
   const reportIdentifiers = reportsToCompare.map(({ identifier }) => identifier);
   const attributes = (action.attributes
@@ -60,33 +101,45 @@ const ReportComparisonBlock = (props) => {
   );
 
   return (
-    <ActionSection className="text-content">
+    <ReportSection className="text-content">
       <Row>
         <Col>
-          <SectionHeader>{block.reportType.name}</SectionHeader>
+          <SectionHeader>
+            <h2>{block.reportType.name}</h2>
+            <ToggleButton
+              color="link"
+              onClick={toggle}
+              className={isOpen ? 'open' : ''}
+            >
+              { isOpen ? t('close') : t('open') }
+              <Icon name={isOpen ? 'angle-down' : 'angle-right'} />
+            </ToggleButton>
+          </SectionHeader>
         </Col>
       </Row>
-      <ReportFieldComparison>
-      { attributes && attributes.map((attribute) => (
-        <ReportField key={attribute.id}>
-          <FieldHeader>
-            <ReportDate>
-              { `${t('report')}: ` }
-              { dayjs(attribute.type.report.startDate).format('l') }
-              {` `}&ndash;{` `}
-              { dayjs(attribute.type.report.endDate).format('l') }
-            </ReportDate>
-            <ReportName>{attribute.type.report.name}</ReportName>
-          </FieldHeader>
-          <ActionAttribute
-            key={attribute.id}
-            attribute={attribute}
-            attributeType={undefined}
-          />
-        </ReportField>
-      ))}
-      </ReportFieldComparison>
-    </ActionSection>
+      <Collapse isOpen={isOpen}>
+        <ReportFieldComparison>
+        { attributes && attributes.map((attribute) => (
+          <ReportField key={attribute.id}>
+            <FieldHeader>
+              <ReportDate>
+                { `${t('report')}: ` }
+                { dayjs(attribute.type.report.startDate).format('l') }
+                {` `}&ndash;{` `}
+                { dayjs(attribute.type.report.endDate).format('l') }
+              </ReportDate>
+              <ReportName>{attribute.type.report.name}</ReportName>
+            </FieldHeader>
+            <ActionAttribute
+              key={attribute.id}
+              attribute={attribute}
+              attributeType={undefined}
+            />
+          </ReportField>
+        ))}
+        </ReportFieldComparison>
+      </Collapse>
+    </ReportSection>
   );
 };
 
