573c59df8b574914189ef924a0d306240ac83c6f
Timo Tuominen <tituomin@gmail.com>
2023-03-31 15:08:42 +0300

    Whitespace cleanup

--- components/dashboard/ActionStatusTable.js
+++ components/dashboard/ActionStatusTable.js
@@ -24,10 +24,10 @@ const TableWrapper = styled.div`
     linear-gradient(to right, rgba(0,0,0,.25), rgba(255,255,255,0)),
     linear-gradient(to left, rgba(0,0,0,.25), rgba(255,255,255,0));
   background-position: left center, right center, left center, right center;
-	background-repeat: no-repeat;
-	background-color: white;
-	background-size: 20px 100%, 20px 100%, 10px 100%, 10px 100%;
-	background-attachment: local, local, scroll, scroll;
+  background-repeat: no-repeat;
+  background-color: white;
+  background-size: 20px 100%, 20px 100%, 10px 100%, 10px 100%;
+  background-attachment: local, local, scroll, scroll;
 `;
 
 const DashTable = styled(Table)`
