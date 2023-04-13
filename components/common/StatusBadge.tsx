5c0365c994d62924c228de570b3b5f7afca998fd
Timo Tuominen <tituomin@gmail.com>
2023-04-13 22:05:58 +0300

    Type annotations

--- components/common/StatusBadge.tsx
+++ components/common/StatusBadge.tsx
@@ -1,33 +1,53 @@
-import React from 'react';
+import React, { PropsWithChildren} from 'react';
 import styled from 'styled-components';
 import { useTheme } from 'common/theme';
+import type { Theme } from '@kausal/themes/types'
+import type { PlanContextType } from 'context/plan';
+import type { ActionStatusSummary } from 'common/__generated__/graphql';
 
 import { getStatusColorForAction } from 'common/ActionStatusSummary';
 
-const StatusBar = styled.div`
-
-background-color: ${(props) => props.theme.themeColors.light};
-
-.color-bar {
-  height: 8px;
-  background-color: ${(props) => props.statusColor};
+interface StatusBarProps {
+  theme: Theme;
+  statusColor: string;
 }
 
-.label {
-  padding: 3px 6px;
-  font-size: ${(props) => props.theme.fontSizeSm};
-  font-family: ${(props) => props.theme.fontFamilyTiny};
-}
+const StyledStatusBar = styled.div<StatusBarProps>`
+  background-color: ${(props) => props.theme.themeColors.light};
+
+  .color-bar {
+    height: 8px;
+    background-color: ${(props) => props.statusColor};
+  }
+
+  .label {
+    padding: 3px 6px;
+    font-size: ${(props) => props.theme.fontSizeSm};
+    font-family: ${(props) => props.theme.fontFamilyTiny};
+  }
 `;
 
-const StatusBadge = (props) => {
+const StatusBar = (props: PropsWithChildren<StatusBarProps>) => {
+  const { theme, statusColor, children } = props;
+  return <StyledStatusBar theme={theme} statusColor={statusColor}>
+    {children}
+  </StyledStatusBar>;
+}
+
+interface StatusBadgeProps {
+  statusSummary: ActionStatusSummary;
+  statusName?: string;
+  plan: PlanContextType;
+}
+
+const StatusBadge = (props: StatusBadgeProps) => {
   const { statusSummary, statusName, plan } = props;
   const theme = useTheme();
-
+  const statusColor = getStatusColorForAction({statusSummary}, plan, theme);
   return (
-    <StatusBar statusColor={getStatusColorForAction({statusSummary}, plan, theme)}>
+    <StatusBar statusColor={statusColor} theme={theme}>
       <div className="color-bar" />
-      <div className="label">{ statusName }</div>
+      <div className="label">{ statusName ?? statusSummary.label }</div>
     </StatusBar>
   );
 };
