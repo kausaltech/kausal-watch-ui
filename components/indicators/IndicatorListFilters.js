fa596d417cc0c96718f74abe51ff864f8d7a5e99
Joe Smallwood <joe.smallwood@kausal.tech>
2023-08-25 17:35:26 +0300

    Refactor indicator filters and utilise action filters

--- components/indicators/IndicatorListFilters.js
+++ components/indicators/IndicatorListFilters.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { cloneDeep } from 'lodash';
 import { Row, Col } from 'reactstrap';
 import { withTheme } from 'styled-components';
+
 import { withTranslation } from '../../common/i18n';
 import TextInput from '../common/TextInput';
 import DropDown from '../common/DropDown';
@@ -34,30 +35,35 @@ class IndicatorListFilters extends React.Component {
 
   getCategoryName(catId) {
     const { cats } = this.props;
-    const category = cats.find(cat => cat.id === catId);
+    const category = cats.find((cat) => cat.id === catId);
     return category ? category.name : t('filter-all-categories');
   }
 
   getCategoryIdentifier(catId) {
     const { cats } = this.props;
-    const category = cats.find(cat => cat.id === catId);
+    const category = cats.find((cat) => cat.id === catId);
     return category ? category.identifier : '';
   }
 
   getDepth(cats, catId, depth) {
     let currentDepth = depth;
     const currentCat = cats.find((cat) => cat.id === catId);
-    if (currentCat.parent) currentDepth = this.getDepth(cats, currentCat.parent.id, depth + 1);
+    if (currentCat.parent)
+      currentDepth = this.getDepth(cats, currentCat.parent.id, depth + 1);
     return currentDepth;
-  };
+  }
 
   render() {
     const { t, theme, cats } = this.props;
     const { activeCat } = this.state;
-    const { showIdentifiers, filterIndicatorsByAllLevels } = theme.settings.categories;
+    const { showIdentifiers, filterIndicatorsByAllLevels } =
+      theme.settings.categories;
 
-    let sortedCats = cloneDeep(cats.sort((a, b) => a.identifier.localeCompare(b.identifier)));
-    if (!filterIndicatorsByAllLevels) sortedCats = sortedCats.filter((cat) => !cat.parent);
+    let sortedCats = cloneDeep(
+      cats.sort((a, b) => a.identifier.localeCompare(b.identifier))
+    );
+    if (!filterIndicatorsByAllLevels)
+      sortedCats = sortedCats.filter((cat) => !cat.parent);
 
     sortedCats.forEach((cat) => {
       cat.label = `${' '.repeat(this.getDepth(cats, cat.id, 0) * 4)}
@@ -66,27 +72,32 @@ class IndicatorListFilters extends React.Component {
     });
 
     return (
-      <form className="filters mb-5 mt-5" onSubmit={(event) => { event.preventDefault(); }}>
+      <form
+        className="filters mb-5 mt-5"
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
         <Row>
-        { sortedCats.length > 1 && (
-          <Col sm="9" md="4" lg="5">
-            <DropDown
-              label={t('filter-category')}
-              type="select"
-              id="catfield"
-              name="category"
-              value={activeCat}
-              onChange={this.onCatBtnClick}
-              className="mb-2"
-            >
-              <option value="">{t('filter-all-categories')}</option>
-              {sortedCats.map(cat => (
-                <option value={cat.id} key={cat.id}>
-                  { cat.label }
-                </option>
-              ))}
-            </DropDown>
-          </Col>
+          {sortedCats.length > 1 && (
+            <Col sm="9" md="4" lg="5">
+              <DropDown
+                label={t('filter-category')}
+                type="select"
+                id="catfield"
+                name="category"
+                value={activeCat}
+                onChange={this.onCatBtnClick}
+                className="mb-2"
+              >
+                <option value="">{t('filter-all-categories')}</option>
+                {sortedCats.map((cat) => (
+                  <option value={cat.id} key={cat.id}>
+                    {cat.label}
+                  </option>
+                ))}
+              </DropDown>
+            </Col>
           )}
           <Col sm="9" md="5" lg="5">
             <TextInput
@@ -97,9 +108,16 @@ class IndicatorListFilters extends React.Component {
               onChange={this.onSearchChange}
             />
           </Col>
-          <Col xs={6} sm={3} md={3} lg={2} xl={2} className="d-flex flex-column justify-content-end">
+          <Col
+            xs={6}
+            sm={3}
+            md={3}
+            lg={2}
+            xl={2}
+            className="d-flex flex-column justify-content-end"
+          >
             <Button type="submit" color="primary" className="mb-3" block>
-              { t('search') }
+              {t('search')}
             </Button>
           </Col>
         </Row>
