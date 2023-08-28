4b64906f85e05d167e1f513e2abb5817078ebe28
Lilia Sviridova <svirivoda@gmail.com>
2023-08-28 10:17:04 +0300

    Added a modal to pie charts

--- components/graphs/StatusDonut.js
+++ components/graphs/StatusDonut.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import dynamic from 'next/dynamic';
-import styled from 'styled-components';
-import { useTheme } from 'common/theme';
-import { useTranslation } from 'common/i18n';
-import Card from 'components/common/Card';
-import ContentLoader from 'components/common/ContentLoader';
+import React, { useState } from "react";
+import dynamic from "next/dynamic";
+import styled from "styled-components";
+import { useTheme } from "common/theme";
+import { useTranslation } from "common/i18n";
+import Card from "components/common/Card";
+import ContentLoader from "components/common/ContentLoader";
+import Modal from "components/common/Modal";
 
 const GraphCard = styled.div`
   display: flex;
@@ -14,6 +15,7 @@ const GraphCard = styled.div`
   padding: 1rem;
   margin: 1rem;
   max-width: 260px;
+  cursor: pointer;
 `;
 
 const GraphHeader = styled.h2`
@@ -30,24 +32,18 @@ const HelpText = styled.p`
   line-height: ${(props) => props.theme.lineHeightMd};
 `;
 
-const Plot = dynamic(
-  () => import('./Plot'),
-  {
-    loading: () => <ContentLoader />,
-    ssr: false,
-  }
-);
+const Plot = dynamic(() => import("./Plot"), {
+  loading: () => <ContentLoader />,
+  ssr: false,
+});
 
 const StatusDonut = (props) => {
-  const {
-    data,
-    currentValue,
-    colors,
-    header,
-    helpText,
-  } = props;
+  const { data, currentValue, colors, header, helpText } = props;
   const theme = useTheme();
   const { i18n } = useTranslation();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
 
   const isServer = typeof window === "undefined";
   if (isServer) {
@@ -58,13 +54,13 @@ const StatusDonut = (props) => {
     values: [...data.values],
     labels: [...data.labels],
     domain: { column: 0 },
-    hoverinfo: 'label+value+percent',
-    hovertemplate: '%{label}<br>%{value}<br>%{percent:.0%}<extra></extra>',
+    hoverinfo: "label+value+percent",
+    hovertemplate: "%{label}<br>%{value}<br>%{percent:.0%}<extra></extra>",
     hole: 0.5,
-    type: 'pie',
+    type: "pie",
     sort: false,
-    direction: 'clockwise',
-    textinfo: 'none',
+    direction: "clockwise",
+    textinfo: "none",
     marker: {
       colors: [...colors],
     },
@@ -88,8 +84,8 @@ const StatusDonut = (props) => {
     height: 175,
     width: 175,
     showlegend: false,
-    paper_bgcolor: 'rgba(0,0,0,0)',
-    margin: {"t": 0, "b": 0, "l": 0, "r": 0},
+    paper_bgcolor: "rgba(0,0,0,0)",
+    margin: { t: 0, b: 0, l: 0, r: 0 },
   };
   const config = {
     displaylogo: false,
@@ -97,19 +93,57 @@ const StatusDonut = (props) => {
     locales: {
       fi: {
         format: {
-          decimal: ',',
-          thousands: ' ',
+          decimal: ",",
+          thousands: " ",
           grouping: [3],
         },
       },
     },
   };
+
+  const pieDataWithPercent = {
+    ...pieData,
+    textinfo: "percent",
+  };
+  const pieLayoutWithLegend = {
+    ...pieLayout,
+    height: 350,
+    width: 350,
+    showlegend: true,
+    legend: {
+      y: 0.9,
+      yanchor: "auto",
+    },
+  };
+  const configNoButton = {
+    ...config,
+    modeBarButtonsToRemove: ["toImage"],
+  };
   return (
-    <GraphCard>
-      <GraphHeader>{header}</GraphHeader>
-      <HelpText>{ helpText }</HelpText>
-      <Plot data={[pieData]} layout={pieLayout} config={config} />
-    </GraphCard>
+    <>
+      <GraphCard onClick={openModal}>
+        <GraphHeader>{header}</GraphHeader>
+        <HelpText>{helpText}</HelpText>
+        <Plot
+          data={[pieData]}
+          layout={pieLayout}
+          config={configNoButton}
+          onClick={openModal}
+        />
+      </GraphCard>
+      <Modal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        header={header}
+        helpText={helpText}
+      >
+        <Plot
+          data={[pieDataWithPercent]}
+          layout={pieLayoutWithLegend}
+          config={config}
+        />
+      </Modal>
+    </>
   );
 };
 
