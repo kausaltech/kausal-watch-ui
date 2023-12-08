cdd77893815706dc3d8b34018db06e1abde78f18
Tero Tikkanen <tero.tikkanen@gmail.com>
2023-12-08 14:42:43 +0200

    Enable graph download, disable zoomin

--- components/graphs/IndicatorGraph.tsx
+++ components/graphs/IndicatorGraph.tsx
@@ -70,6 +70,7 @@ const createLayout = (
     ? {
         xaxis: {
           automargin: true,
+          fixedrange: true,
           type: 'category',
           tickangle: subplotsNeeded ? 'auto' : 90,
           tickfont: {
@@ -81,9 +82,9 @@ const createLayout = (
     : {
         xaxis: {
           automargin: true,
+          fixedrange: true,
           showgrid: false,
           showline: false,
-          fixedrange: false,
           nticks: config?.xTicksMax,
           tickformat: timeResolution === 'YEAR' ? '%Y' : '%b %Y',
           tickmode: 'auto',
@@ -119,6 +120,23 @@ const createLayout = (
     // showlegend: false,
     hoverlabel: {
       namelength: 0,
+    },
+    modebar: {
+      add: ['toImage'],
+      remove: [
+        'zoom2d',
+        'zoomIn2d',
+        'zoomOut2d',
+        'pan2d',
+        'select2d',
+        'lasso2d',
+        'autoScale2d',
+        'resetScale2d',
+        'sendDataToCloud',
+      ],
+      color: theme.graphColors.grey090,
+      bgcolor: theme.graphColors.grey010,
+      activecolor: theme.brandDark,
     },
   };
 
@@ -508,8 +526,7 @@ function IndicatorGraph(props: IndicatorGraphProps) {
           document.dispatchEvent(event);
         }}
         config={{
-          displayModeBar: false,
-          showSendToCloud: true,
+          displaylogo: false,
           staticPlot: false,
         }}
       />
