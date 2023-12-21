0319e2d635a51c40636f8cd4d3f504471f676a72
Timo Tuominen <tituomin@gmail.com>
2023-12-21 11:37:27 +0200

    Fix bug which hid version banners in production

--- components/common/ApplicationStateBanner.js
+++ components/common/ApplicationStateBanner.js
@@ -47,14 +47,15 @@ function ApplicationStateBanner(props) {
   const plan = usePlan();
 
   // Only show superseding versions in production if they are published
-  const supersedingVersions =
-    deploymentType !== 'production'
-      ? plan.supersedingPlans
-      : plan.supersedingPlans.filter((p) => p.publishedAt);
+  let supersedingVersions = plan.supersedingPlans;
+  if (deploymentType === 'production') {
+    supersedingVersions = supersedingVersions.filter((p) => p.publishedAt);
+  }
 
   switch (deploymentType) {
     case 'production':
-      return null;
+      typeLabel = null;
+      break;
     case 'testing':
       typeLabel = t('instance-type-testing-label');
       typeMessage = t('instance-type-testing-message');
@@ -67,14 +68,18 @@ function ApplicationStateBanner(props) {
       typeLabel = t('instance-type-development-label');
       typeMessage = t('instance-type-development-message');
   }
-
-  return (
-    <>
+  const banner =
+    typeLabel != null ? (
       <Banner>
         <Label type={deploymentType}>{typeLabel.toUpperCase()}</Label>
         {` ${typeMessage}`}
       </Banner>
-      {supersedingVersions?.length > 0 && (
+    ) : null;
+
+  return (
+    <>
+      { banner }
+      { supersedingVersions?.length > 0 && (
         <PlanVersionBanner
           currentVersion={plan}
           latestVersion={supersedingVersions[supersedingVersions.length - 1]}
