5e80a7737945933155a36d862288a51c935dfc7a
Joe Smallwood <joe.smallwood@kausal.tech>
2024-01-09 15:30:33 +0200

    Support client side use of deployment type env var

--- common/environment.ts
+++ common/environment.ts
@@ -1,6 +1,7 @@
 export const isServer = typeof window === 'undefined';
 
-export const deploymentType = process.env.DEPLOYMENT_TYPE || 'development';
+export const deploymentType =
+  process.env.NEXT_PUBLIC_DEPLOYMENT_TYPE || 'development';
 
 export const isLocal = process.env.NODE_ENV === 'development';
 
