448ce2cd1adc8b1e7907a19fb4913c9312c29c3c
Joe Smallwood <joe.smallwood@kausal.tech>
2024-01-11 15:33:45 +0200

    Disallow crawlers in non-production environments

--- common/environment.ts
+++ common/environment.ts
@@ -1,7 +1,15 @@
+type DeploymentType =
+  | 'production'
+  | 'staging'
+  | 'development'
+  | 'testing'
+  | 'wip';
+
 export const isServer = typeof window === 'undefined';
 
-export const deploymentType =
-  process.env.NEXT_PUBLIC_DEPLOYMENT_TYPE || 'development';
+export const deploymentType: DeploymentType =
+  (process.env.NEXT_PUBLIC_DEPLOYMENT_TYPE as DeploymentType | undefined) ||
+  'development';
 
 export const isLocal = process.env.NODE_ENV === 'development';
 
@@ -9,9 +17,3 @@ export const apiUrl =
   process.env.NEXT_PUBLIC_API_URL || 'https://api.watch.kausal.tech/v1';
 
 export const gqlUrl = `${apiUrl}/graphql/`;
-
-console.log(
-  'TEST ENVS',
-  process.env.NEXT_PUBLIC_API_URL,
-  process.env.NEXT_PUBLIC_DEPLOYMENT_TYPE
-);
