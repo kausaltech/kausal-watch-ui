20c1a732be7daf1d5569a7a6078b529aee9867fd
Joe Smallwood <joe.smallwood@kausal.tech>
2024-01-16 17:03:35 +0200

    Add intl provider for tests

--- tests/test-utils.js
+++ tests/test-utils.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
+import { NextIntlClientProvider } from 'next-intl';
+import commonMessages from '@/locales/en/common.json';
+import a11yMessages from '@/locales/en/a11y.json';
+import actionsMessages from '@/locales/en/actions.json';
 import theme from '../public/static/themes/default/theme.json';
 
-const AllTheProviders = ({ children }) => (
-  <ThemeProvider theme={theme}>{children}</ThemeProvider>
-);
+const messages = { ...a11yMessages, ...actionsMessages, ...commonMessages };
+
+const AllTheProviders = ({ children }) => {
+  return (
+    <NextIntlClientProvider locale={'en'} messages={messages}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </NextIntlClientProvider>
+  );
+};
 
 const customRender = (ui, options) =>
   render(ui, { wrapper: AllTheProviders, ...options });
