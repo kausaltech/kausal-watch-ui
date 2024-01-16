e9d7cd36c2cdf9ca090755683a9d03a721261adb
Joe Smallwood <joe.smallwood@kausal.tech>
2024-01-16 17:32:59 +0200

    Fix missing theme in root loading component

--- app/[domain]/[lang]/layout.tsx
+++ app/[domain]/[lang]/layout.tsx
@@ -3,6 +3,8 @@ import { NextIntlClientProvider, useMessages } from 'next-intl';
 
 import { StyledComponentsRegistry } from '@/lib/StyledComponentsRegistry';
 import { ApolloWrapper } from '@/lib/ApolloWrapper';
+import ThemeProvider from '@/lib/ThemeProvider';
+import defaultTheme from '@/public/static/themes/default/theme.json';
 import '@/styles/default/main.scss';
 
 type Props = {
@@ -16,11 +18,15 @@ export default function LangLayout({ params, children }: Props) {
   return (
     <html lang={params.lang}>
       <body>
-        <NextIntlClientProvider locale={params.lang} messages={messages}>
-          <StyledComponentsRegistry>
-            <ApolloWrapper>{children}</ApolloWrapper>
-          </StyledComponentsRegistry>
-        </NextIntlClientProvider>
+        {/* Initially provide the default theme since the plan theme identifier is loaded asynchronously.
+            This prevents errors in root components such as loaders that require a theme */}
+        <ThemeProvider theme={defaultTheme}>
+          <NextIntlClientProvider locale={params.lang} messages={messages}>
+            <StyledComponentsRegistry>
+              <ApolloWrapper>{children}</ApolloWrapper>
+            </StyledComponentsRegistry>
+          </NextIntlClientProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
