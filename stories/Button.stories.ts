ff1ac4a3c8003352b66ce1aa107ee4f536eb6c25
Tero Tikkanen <tero.tikkanen@gmail.com>
2024-02-21 11:25:54 +0200

    Cleanup

--- stories/Button.stories.ts
+++ stories/Button.stories.ts
@@ -2,24 +2,19 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import Button from '@/components/common/Button';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Basic/Button',
   component: Button,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {},
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
     outline: false,
