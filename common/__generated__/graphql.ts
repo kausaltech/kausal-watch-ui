e35e2132d07accc941e29352f2baeefa1a78ecdd
Joe Smallwood <joe.smallwood@kausal.tech>
2024-02-23 14:37:29 +0200

    Use new workflow query

--- common/__generated__/graphql.ts
+++ common/__generated__/graphql.ts
@@ -309,11 +309,12 @@ export type ActionCategoryReportValue = ReportValueInterface & {
   field: ReportFieldBlock;
 };
 
-export type ActionContactFormBlock = StreamFieldInterface & {
+export type ActionContactFormBlock = FieldBlockMetaInterface & StreamFieldInterface & {
   __typename?: 'ActionContactFormBlock';
   blockType: Scalars['String'];
   field: Scalars['String'];
   id?: Maybe<Scalars['String']>;
+  meta?: Maybe<FieldBlockMetaData>;
   rawValue: Scalars['String'];
   value: Scalars['String'];
 };
@@ -3409,6 +3410,7 @@ export type Query = {
   plansForHostname?: Maybe<Array<Maybe<PlanInterface>>>;
   relatedPlanActions?: Maybe<Array<Action>>;
   search?: Maybe<SearchResults>;
+  workflowStates?: Maybe<Array<Maybe<WorkflowStateDescription>>>;
 };
 
 
@@ -4176,112 +4178,11 @@ export enum WorkflowState {
   Published = 'PUBLISHED'
 }
 
-/** One possible value for a given Enum. Enum values are unique values, not a placeholder for a string or numeric value. However an Enum value is returned in a JSON response as a string. */
-export type __EnumValue = {
-  __typename?: '__EnumValue';
-  name: Scalars['String'];
+export type WorkflowStateDescription = {
+  __typename?: 'WorkflowStateDescription';
   description?: Maybe<Scalars['String']>;
-  isDeprecated: Scalars['Boolean'];
-  deprecationReason?: Maybe<Scalars['String']>;
+  id?: Maybe<WorkflowState>;
 };
-
-/** Object and Interface types are described by a list of Fields, each of which has a name, potentially a list of arguments, and a return type. */
-export type __Field = {
-  __typename?: '__Field';
-  name: Scalars['String'];
-  description?: Maybe<Scalars['String']>;
-  args: Array<__InputValue>;
-  type: __Type;
-  isDeprecated: Scalars['Boolean'];
-  deprecationReason?: Maybe<Scalars['String']>;
-};
-
-
-/** Object and Interface types are described by a list of Fields, each of which has a name, potentially a list of arguments, and a return type. */
-export type __FieldArgsArgs = {
-  includeDeprecated?: InputMaybe<Scalars['Boolean']>;
-};
-
-/** Arguments provided to Fields or Directives and the input fields of an InputObject are represented as Input Values which describe their type and optionally a default value. */
-export type __InputValue = {
-  __typename?: '__InputValue';
-  name: Scalars['String'];
-  description?: Maybe<Scalars['String']>;
-  type: __Type;
-  /** A GraphQL-formatted string representing the default value for this input value. */
-  defaultValue?: Maybe<Scalars['String']>;
-  isDeprecated: Scalars['Boolean'];
-  deprecationReason?: Maybe<Scalars['String']>;
-};
-
-/**
- * The fundamental unit of any GraphQL Schema is the type. There are many kinds of types in GraphQL as represented by the `__TypeKind` enum.
- *
- * Depending on the kind of a type, certain fields describe information about that type. Scalar types provide no information beyond a name, description and optional `specifiedByUrl`, while Enum types provide their values. Object and Interface types provide the fields they describe. Abstract types, Union and Interface, provide the Object types possible at runtime. List and NonNull types compose other types.
- */
-export type __Type = {
-  __typename?: '__Type';
-  kind: __TypeKind;
-  name?: Maybe<Scalars['String']>;
-  description?: Maybe<Scalars['String']>;
-  specifiedByUrl?: Maybe<Scalars['String']>;
-  fields?: Maybe<Array<__Field>>;
-  interfaces?: Maybe<Array<__Type>>;
-  possibleTypes?: Maybe<Array<__Type>>;
-  enumValues?: Maybe<Array<__EnumValue>>;
-  inputFields?: Maybe<Array<__InputValue>>;
-  ofType?: Maybe<__Type>;
-};
-
-
-/**
- * The fundamental unit of any GraphQL Schema is the type. There are many kinds of types in GraphQL as represented by the `__TypeKind` enum.
- *
- * Depending on the kind of a type, certain fields describe information about that type. Scalar types provide no information beyond a name, description and optional `specifiedByUrl`, while Enum types provide their values. Object and Interface types provide the fields they describe. Abstract types, Union and Interface, provide the Object types possible at runtime. List and NonNull types compose other types.
- */
-export type __TypeFieldsArgs = {
-  includeDeprecated?: InputMaybe<Scalars['Boolean']>;
-};
-
-
-/**
- * The fundamental unit of any GraphQL Schema is the type. There are many kinds of types in GraphQL as represented by the `__TypeKind` enum.
- *
- * Depending on the kind of a type, certain fields describe information about that type. Scalar types provide no information beyond a name, description and optional `specifiedByUrl`, while Enum types provide their values. Object and Interface types provide the fields they describe. Abstract types, Union and Interface, provide the Object types possible at runtime. List and NonNull types compose other types.
- */
-export type __TypeEnumValuesArgs = {
-  includeDeprecated?: InputMaybe<Scalars['Boolean']>;
-};
-
-
-/**
- * The fundamental unit of any GraphQL Schema is the type. There are many kinds of types in GraphQL as represented by the `__TypeKind` enum.
- *
- * Depending on the kind of a type, certain fields describe information about that type. Scalar types provide no information beyond a name, description and optional `specifiedByUrl`, while Enum types provide their values. Object and Interface types provide the fields they describe. Abstract types, Union and Interface, provide the Object types possible at runtime. List and NonNull types compose other types.
- */
-export type __TypeInputFieldsArgs = {
-  includeDeprecated?: InputMaybe<Scalars['Boolean']>;
-};
-
-/** An enum describing what kind of type a given `__Type` is. */
-export enum __TypeKind {
-  /** Indicates this type is a scalar. */
-  Scalar = 'SCALAR',
-  /** Indicates this type is an object. `fields` and `interfaces` are valid fields. */
-  Object = 'OBJECT',
-  /** Indicates this type is an interface. `fields`, `interfaces`, and `possibleTypes` are valid fields. */
-  Interface = 'INTERFACE',
-  /** Indicates this type is a union. `possibleTypes` is a valid field. */
-  Union = 'UNION',
-  /** Indicates this type is an enum. `enumValues` is a valid field. */
-  Enum = 'ENUM',
-  /** Indicates this type is an input object. `inputFields` is a valid field. */
-  InputObject = 'INPUT_OBJECT',
-  /** Indicates this type is a list. `ofType` is a valid field. */
-  List = 'LIST',
-  /** Indicates this type is a non-null. `ofType` is a valid field. */
-  NonNull = 'NON_NULL'
-}
 
 export type GetSitemapQueryVariables = Exact<{
   id: Scalars['ID'];
@@ -4659,13 +4560,10 @@ export type GetWorkflowsQueryVariables = Exact<{ [key: string]: never; }>;
 
 
 export type GetWorkflowsQuery = (
-  { __type?: (
-    { name?: string | null, description?: string | null, enumValues?: Array<(
-      { name: string, description?: string | null }
-      & { __typename?: '__EnumValue' }
-    )> | null }
-    & { __typename?: '__Type' }
-  ) | null }
+  { workflowStates?: Array<(
+    { id?: WorkflowState | null, description?: string | null }
+    & { __typename?: 'WorkflowStateDescription' }
+  ) | null> | null }
   & { __typename?: 'Query' }
 );
 
@@ -7462,8 +7360,11 @@ export type GetActionDetailsQuery = (
   ) | null, plan?: (
     { actionListPage?: (
       { actionDateFormat?: string | null, taskDateFormat?: string | null, detailsMainTop?: Array<(
-        { id?: string | null }
-        & { __typename: 'ActionContactFormBlock' }
+        { id?: string | null, meta?: (
+          { restricted?: boolean | null, hidden?: boolean | null }
+          & { __typename?: 'FieldBlockMetaData' }
+        ) | null }
+        & { __typename: 'ActionContactFormBlock' | 'ActionDescriptionBlock' | 'ActionLeadParagraphBlock' | 'ActionLinksBlock' | 'ActionMergedActionsBlock' | 'ActionRelatedActionsBlock' | 'ActionRelatedIndicatorsBlock' | 'ActionTasksBlock' }
       ) | (
         { id?: string | null, meta?: (
           { restricted?: boolean | null, hidden?: boolean | null }
@@ -7737,12 +7638,6 @@ export type GetActionDetailsQuery = (
           & { __typename?: 'ReportComparisonBlock' }
         ) | null> | null }
         & { __typename: 'ActionContentSectionBlock' }
-      ) | (
-        { id?: string | null, meta?: (
-          { restricted?: boolean | null, hidden?: boolean | null }
-          & { __typename?: 'FieldBlockMetaData' }
-        ) | null }
-        & { __typename: 'ActionDescriptionBlock' | 'ActionLeadParagraphBlock' | 'ActionLinksBlock' | 'ActionMergedActionsBlock' | 'ActionRelatedActionsBlock' | 'ActionRelatedIndicatorsBlock' | 'ActionTasksBlock' }
       ) | (
         { id?: string | null, fieldLabel?: string | null, caption?: string | null, meta?: (
           { restricted?: boolean | null, hidden?: boolean | null }
@@ -7954,8 +7849,11 @@ export type GetActionDetailsQuery = (
         ) | null> | null }
         & { __typename: 'ReportComparisonBlock' }
       )> | null, detailsMainBottom?: Array<(
-        { id?: string | null }
-        & { __typename: 'ActionContactFormBlock' }
+        { id?: string | null, meta?: (
+          { restricted?: boolean | null, hidden?: boolean | null }
+          & { __typename?: 'FieldBlockMetaData' }
+        ) | null }
+        & { __typename: 'ActionContactFormBlock' | 'ActionDescriptionBlock' | 'ActionLeadParagraphBlock' | 'ActionLinksBlock' | 'ActionMergedActionsBlock' | 'ActionRelatedActionsBlock' | 'ActionRelatedIndicatorsBlock' | 'ActionTasksBlock' }
       ) | (
         { id?: string | null, meta?: (
           { restricted?: boolean | null, hidden?: boolean | null }
@@ -8229,12 +8127,6 @@ export type GetActionDetailsQuery = (
           & { __typename?: 'ReportComparisonBlock' }
         ) | null> | null }
         & { __typename: 'ActionContentSectionBlock' }
-      ) | (
-        { id?: string | null, meta?: (
-          { restricted?: boolean | null, hidden?: boolean | null }
-          & { __typename?: 'FieldBlockMetaData' }
-        ) | null }
-        & { __typename: 'ActionDescriptionBlock' | 'ActionLeadParagraphBlock' | 'ActionLinksBlock' | 'ActionMergedActionsBlock' | 'ActionRelatedActionsBlock' | 'ActionRelatedIndicatorsBlock' | 'ActionTasksBlock' }
       ) | (
         { id?: string | null, fieldLabel?: string | null, caption?: string | null, meta?: (
           { restricted?: boolean | null, hidden?: boolean | null }
@@ -8553,9 +8445,12 @@ type ActionAsideContentBlocksFragment_ActionResponsiblePartiesBlock_Fragment = (
 
 export type ActionAsideContentBlocksFragmentFragment = ActionAsideContentBlocksFragment_ActionContactPersonsBlock_ActionScheduleBlock_Fragment | ActionAsideContentBlocksFragment_ActionContentAttributeTypeBlock_Fragment | ActionAsideContentBlocksFragment_ActionContentCategoryTypeBlock_Fragment | ActionAsideContentBlocksFragment_ActionResponsiblePartiesBlock_Fragment;
 
-type ActionMainContentBlocksFragment_ActionContactFormBlock_Fragment = (
-  { id?: string | null }
-  & { __typename: 'ActionContactFormBlock' }
+type ActionMainContentBlocksFragment_FVKamQUfmT6JjFbafBAu3ifcqx9spC4XsfbOqsBaoA_Fragment = (
+  { id?: string | null, meta?: (
+    { restricted?: boolean | null, hidden?: boolean | null }
+    & { __typename?: 'FieldBlockMetaData' }
+  ) | null }
+  & { __typename: 'ActionContactFormBlock' | 'ActionDescriptionBlock' | 'ActionLeadParagraphBlock' | 'ActionLinksBlock' | 'ActionMergedActionsBlock' | 'ActionRelatedActionsBlock' | 'ActionRelatedIndicatorsBlock' | 'ActionTasksBlock' }
 );
 
 type ActionMainContentBlocksFragment_ActionContentAttributeTypeBlock_Fragment = (
@@ -8837,14 +8732,6 @@ type ActionMainContentBlocksFragment_ActionContentSectionBlock_Fragment = (
   & { __typename: 'ActionContentSectionBlock' }
 );
 
-type ActionMainContentBlocksFragment_6BcBJmD8IFwBu5Wuh9zsZjz_45yKhLxTzz6qM1g7V4_Fragment = (
-  { id?: string | null, meta?: (
-    { restricted?: boolean | null, hidden?: boolean | null }
-    & { __typename?: 'FieldBlockMetaData' }
-  ) | null }
-  & { __typename: 'ActionDescriptionBlock' | 'ActionLeadParagraphBlock' | 'ActionLinksBlock' | 'ActionMergedActionsBlock' | 'ActionRelatedActionsBlock' | 'ActionRelatedIndicatorsBlock' | 'ActionTasksBlock' }
-);
-
 type ActionMainContentBlocksFragment_ActionOfficialNameBlock_Fragment = (
   { id?: string | null, fieldLabel?: string | null, caption?: string | null, meta?: (
     { restricted?: boolean | null, hidden?: boolean | null }
@@ -9059,7 +8946,7 @@ type ActionMainContentBlocksFragment_ReportComparisonBlock_Fragment = (
   & { __typename: 'ReportComparisonBlock' }
 );
 
-export type ActionMainContentBlocksFragmentFragment = ActionMainContentBlocksFragment_ActionContactFormBlock_Fragment | ActionMainContentBlocksFragment_ActionContentAttributeTypeBlock_Fragment | ActionMainContentBlocksFragment_ActionContentCategoryTypeBlock_Fragment | ActionMainContentBlocksFragment_ActionContentSectionBlock_Fragment | ActionMainContentBlocksFragment_6BcBJmD8IFwBu5Wuh9zsZjz_45yKhLxTzz6qM1g7V4_Fragment | ActionMainContentBlocksFragment_ActionOfficialNameBlock_Fragment | ActionMainContentBlocksFragment_ReportComparisonBlock_Fragment;
+export type ActionMainContentBlocksFragmentFragment = ActionMainContentBlocksFragment_FVKamQUfmT6JjFbafBAu3ifcqx9spC4XsfbOqsBaoA_Fragment | ActionMainContentBlocksFragment_ActionContentAttributeTypeBlock_Fragment | ActionMainContentBlocksFragment_ActionContentCategoryTypeBlock_Fragment | ActionMainContentBlocksFragment_ActionContentSectionBlock_Fragment | ActionMainContentBlocksFragment_ActionOfficialNameBlock_Fragment | ActionMainContentBlocksFragment_ReportComparisonBlock_Fragment;
 
 export type ReportComparisonBlockActionContentFragment = (
   { reportField?: string | null, reportType?: (
