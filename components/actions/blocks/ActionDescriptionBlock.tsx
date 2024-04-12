8f629a387e329d38375712ed345e11dce941d84f
Lilia Sviridova <svirivoda@gmail.com>
2024-04-12 15:18:01 +0300

    Implement conditional visibility for ActionDescriptionBlock header

--- components/actions/blocks/ActionDescriptionBlock.tsx
+++ components/actions/blocks/ActionDescriptionBlock.tsx
@@ -1,14 +1,21 @@
 import { ActionSection } from 'components/actions/ActionContent';
 import RichText from 'components/common/RichText';
 import { useTranslations } from 'next-intl';
+import { useTheme } from 'styled-components';
 
 const ActionDescriptionBlock = (props) => {
   const { content } = props;
   const t = useTranslations();
+  const theme = useTheme();
+
+  const headerClass =
+    content && theme.settings.showActionDescriptionHeader
+      ? ''
+      : 'visually-hidden';
 
   return (
     <ActionSection className="text-content">
-      <h2 className="visually-hidden">{t('action-description')}</h2>
+      <h2 className={headerClass}>{t('action-description')}</h2>
       <RichText html={content} />
     </ActionSection>
   );
