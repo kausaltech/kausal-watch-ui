1e795d2490d1a25045ea597ad5600e01cc15197c
Lilia Sviridova <svirivoda@gmail.com>
2024-04-16 16:35:30 +0300

    Add conditional visibility of feedback link in Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -111,16 +111,18 @@ function Footer() {
     utilityLinks.push({ id: '1', name: t('contact'), slug: plan.contactLink });
   }
 
-  if (plan.externalFeedbackUrl) {
-    utilityLinks.push({
-      id: '2',
-      name: t('give-feedback'),
-      slug: plan.externalFeedbackUrl,
-    });
-  } else if (pathname !== '/feedback') {
-    const url = getFeedbackUrl(pathname);
-    if (url != null) {
-      utilityLinks.push({ id: '2', name: t('give-feedback'), slug: url });
+  if (theme.showFeedbackLink !== false) {
+    if (plan.externalFeedbackUrl) {
+      utilityLinks.push({
+        id: '2',
+        name: t('give-feedback'),
+        slug: plan.externalFeedbackUrl,
+      });
+    } else if (pathname !== '/feedback') {
+      const url = getFeedbackUrl(pathname);
+      if (url != null) {
+        utilityLinks.push({ id: '2', name: t('give-feedback'), slug: url });
+      }
     }
   }
 
