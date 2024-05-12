6bc3609233511202dbf2310ada3b7f05841e9a17
Timo Tuominen <tituomin@gmail.com>
2024-05-12 23:06:08 +0300

    Display action contact person block in pink if restricted

--- components/actions/blocks/ActionContactPersonsBlock.tsx
+++ components/actions/blocks/ActionContactPersonsBlock.tsx
@@ -1,12 +1,19 @@
+import { usePlan } from 'context/plan';
+import RestrictedBlockWrapper from './RestrictedBlockWrapper';
 import { ActionSection } from 'components/actions/ActionContent';
 import ContactPersons from 'components/actions/ContactPersons';
 
 const ActionContactPersonsBlock = (props) => {
   const { contactPersons } = props;
+  const plan = usePlan();
+  const isRestricted =
+    plan.features.contactPersonsPublicData === 'ALL_FOR_AUTHENTICATED';
 
   return (
     <ActionSection>
-      <ContactPersons persons={contactPersons.map((item) => item.person)} />
+      <RestrictedBlockWrapper isRestricted={isRestricted} isHidden={false}>
+        <ContactPersons persons={contactPersons.map((item) => item.person)} />
+      </RestrictedBlockWrapper>
     </ActionSection>
   );
 };
