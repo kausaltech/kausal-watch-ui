3916c9ee63136b8e749af4807a5ced646afe6e83
Timo Tuominen <tituomin@gmail.com>
2024-06-03 11:25:50 +0300

    Automatically logout when the session has expired
    Prevents the UI ending up in a logged in state but unable
    to actually retrieve restricted data.

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,4 +1,14 @@
 import { DefaultSession } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
+
+declare module 'next-auth/jwt' {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    /** OpenID ID Token */
+    idToken?: string;
+    expires?: string;
+  }
+}
 
 declare module 'next-auth' {
   interface Session {
