0a9e0cb77ed09c3d39e9d8610ce967346c7dc8ec
Juha Yrjölä <juha.yrjola@iki.fi>
2024-07-16 14:00:57 +0300

    WIP: Start fixing e2e tests

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -1,10 +1,13 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import { defineConfig, devices } from '@playwright/test';
 import dotenv from 'dotenv';
-import { displayConfiguration } from './context';
 
-dotenv.config();
-
-displayConfiguration();
+const basePath = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({
+  path: [path.resolve(basePath, '.env'), path.resolve(basePath, '..', '.env')],
+});
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -20,7 +23,12 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   //workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    //[process.env.CI ? 'github' : 'list'],
+    ['list'],
+    ['json', { outputFile: 'test-results.json' }],
+    ['html', { open: process.env.CI ? 'never' : 'on-failure' }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -28,13 +36,11 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    actionTimeout: 2000,
     screenshot: 'on',
   },
   maxFailures: 10,
-  expect: {
-    timeout: 10000,
-  },
-  timeout: 20000,
+  globalSetup: path.resolve('./global-setup'),
 
   /* Configure projects for major browsers */
   projects: [
@@ -42,14 +48,17 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
+
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
+
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
@@ -71,10 +80,17 @@ export default defineConfig({
     // },
   ],
 
-  /* Run your local dev server before starting the tests 
-  webServer: {
-    command: process.env.TEST_DEVSERVER ? 'yarn dev' : 'yarn start',
-    url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI,
-  },*/
+  /* Run your local dev server before starting the tests */
+  webServer: process.env.TEST_PAGE_BASE_URL
+    ? undefined
+    : {
+        command: process.env.TEST_DEVSERVER
+          ? 'cd .. && npm run dev'
+          : 'cd .. && npm start',
+        url: 'http://localhost:3000/_health',
+        reuseExistingServer: true,
+        env: {
+          NEXT_PUBLIC_WILDCARD_DOMAINS: 'localhost',
+        },
+      },
 });
