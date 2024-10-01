eefd1991e580ae6b5800d7e8b8dec7c95dd6b8da
Tero Tikkanen <tero.tikkanen@gmail.com>
2024-10-02 00:09:34 +0300

    Add categorycard that uses pathsnode

---
+++ components/paths/CategoryCard.tsx
@@ -0,0 +1,189 @@
+import { beautifyValue } from 'common/data/format';
+import { Link } from 'common/links';
+import Card from 'components/common/Card';
+import ActionParameters from 'components/paths/ActionParameters';
+import { useTranslations } from 'next-intl';
+import { readableColor } from 'polished';
+import styled from 'styled-components';
+
+import { activeGoalVar, yearRangeVar } from '@/context/paths/cache';
+import { GET_PATHS_ACTION } from '@/queries/paths/get-paths-actions';
+import {
+  GET_NODE_CONTENT,
+  GET_NODE_INFO,
+} from '@/queries/paths/get-paths-node';
+import { DimensionalMetric } from '@/utils/paths/metric';
+import { getHttpHeaders } from '@/utils/paths/paths.utils';
+import PathsActionNode from '@/utils/paths/PathsActionNode';
+import { useQuery, useReactiveVar } from '@apollo/client';
+
+const GroupIdentifierHeader = styled.div`
+  background-color: ${(props) => props.$color};
+  color: ${(props) => readableColor(props.$color || '#ffffff')};
+  padding: 6px;
+  margin-bottom: 12px;
+`;
+
+const CardHeader = styled.h3`
+  color: ${(props) => props.theme.neutralDark};
+  font-size: ${(props) => props.theme.fontSizeMd};
+  line-height: ${(props) => props.theme.lineHeightMd};
+`;
+
+const Identifier = styled.span`
+  color: ${(props) => props.theme.textColor.tertiary};
+`;
+
+const PathsBasicNodeContent = (props) => {
+  const { node, pathsInstance } = props;
+  const yearRange = useReactiveVar(yearRangeVar);
+  const activeGoal = useReactiveVar(activeGoalVar);
+  const t = useTranslations();
+
+  const { data, loading, error } = useQuery(GET_NODE_CONTENT, {
+    fetchPolicy: 'no-cache',
+    variables: { node: node, goal: activeGoal?.id },
+    context: {
+      uri: '/api/graphql-paths',
+      headers: getHttpHeaders({ instanceIdentifier: pathsInstance }),
+    },
+  });
+
+  if (loading) {
+    return <div>Loading...</div>; // Or any loading indicator you prefer
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>; // Handle error appropriately
+  }
+  if (data) {
+    if (data.node.metricDim) {
+      //console.log('basicnodedata', data);
+      const nodeMetric = new DimensionalMetric(data.node.metricDim!);
+      //console.log('nodeMetric', nodeMetric);
+      //console.log('data from year', nodeMetric.getSingleYear(yearRange[1], {}));
+      const thisYear = nodeMetric.getSingleYear(yearRange[1], {});
+      const directLabel = thisYear.allLabels.find(
+        (label) =>
+          label.id === 'transportation_emissions:emission_scope:group:direct'
+      ).label;
+      const indirectLabel = thisYear.allLabels.find(
+        (label) =>
+          label.id === 'transportation_emissions:emission_scope:group:indirect'
+      ).label;
+      return (
+        <div>
+          <div>{nodeMetric.getName()}</div>
+          <div>
+            {directLabel}: {thisYear.rows[0][0]}
+          </div>
+          <div>
+            {indirectLabel}:{thisYear.rows[0][1]}
+          </div>
+        </div>
+      );
+    } else {
+      return <div>{data.node.__typename} not supported</div>;
+    }
+  }
+  return null;
+};
+
+const PathsActionNodeContent = (props) => {
+  const { node, pathsInstance } = props;
+  const yearRange = useReactiveVar(yearRangeVar);
+  const activeGoal = useReactiveVar(activeGoalVar);
+  const t = useTranslations();
+
+  const { data, loading, error } = useQuery(GET_PATHS_ACTION, {
+    fetchPolicy: 'no-cache',
+    variables: { action: node, goal: activeGoal?.id },
+    context: {
+      uri: '/api/graphql-paths',
+      headers: getHttpHeaders({ instanceIdentifier: pathsInstance }),
+    },
+  });
+
+  if (loading) {
+    return <div>Loading...</div>; // Or any loading indicator you prefer
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>; // Handle error appropriately
+  }
+  if (data) {
+    const pathsAction = new PathsActionNode(data.action);
+    return (
+      <div>
+        {t('impact')} {yearRange[1]}
+        <h4>
+          {yearRange ? (
+            beautifyValue(pathsAction.getYearlyImpact(yearRange[1]))
+          ) : (
+            <span>---</span>
+          )}
+          {pathsAction.getUnit()}
+        </h4>
+        <ActionParameters parameters={data.action.parameters} />
+      </div>
+    );
+  }
+  return null;
+};
+
+const PathsNodeContent = (props) => {
+  const { node, paths } = props;
+
+  const { data, loading, error } = useQuery(GET_NODE_INFO, {
+    fetchPolicy: 'no-cache',
+    variables: { node: node },
+    context: {
+      uri: '/api/graphql-paths',
+      headers: getHttpHeaders({ instanceIdentifier: paths }),
+    },
+  });
+
+  if (loading) {
+    return <div>Loading...</div>; // Or any loading indicator you prefer
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>; // Handle error appropriately
+  }
+  if (data) {
+    if (data.node.__typename === 'ActionNode') {
+      return <PathsActionNodeContent node={node} pathsInstance={paths} />;
+    } else if (data.node.__typename) {
+      return <PathsBasicNodeContent node={node} pathsInstance={paths} />;
+    }
+    return null;
+  }
+};
+
+const CategoryCard = (props) => {
+  const { category, group, pathsInstance } = props;
+  return (
+    <Card>
+      <GroupIdentifierHeader $color="#333">{group.name}</GroupIdentifierHeader>
+      <div>
+        {' '}
+        <Link href={category.categoryPage.urlPath} legacyBehavior>
+          <a className="card-wrapper">
+            <CardHeader className="card-title">
+              {!category?.type.hideCategoryIdentifiers && (
+                <Identifier>{category.identifier}. </Identifier>
+              )}
+              {category.name}
+            </CardHeader>
+          </a>
+        </Link>
+        {category.leadParagraph && <p>{category.leadParagraph}</p>}
+        {category.kausalPathsNodeUuid && (
+          <PathsNodeContent
+            node={category.kausalPathsNodeUuid}
+            paths={pathsInstance}
+          />
+        )}
+      </div>
+    </Card>
+  );
+};
+
+export default CategoryCard;
