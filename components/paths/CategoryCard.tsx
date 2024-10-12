162d2a5bd497153eb1c2f364d2f55e15a4372214
Tero Tikkanen <tero.tikkanen@gmail.com>
2024-10-12 23:33:08 +0300

    Handle sorting of dynamically loaded cards

--- components/paths/CategoryCard.tsx
+++ components/paths/CategoryCard.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import { beautifyValue } from 'common/data/format';
 import { Link } from 'common/links';
 import ActionParameters from 'components/paths/ActionParameters';
@@ -56,7 +58,7 @@ const Identifier = styled.span`
 `;
 
 const PathsBasicNodeContent = (props) => {
-  const { node, pathsInstance } = props;
+  const { categoryId, node, pathsInstance, onLoaded } = props;
   const yearRange = useReactiveVar(yearRangeVar);
   const activeGoal = useReactiveVar(activeGoalVar);
   const t = useTranslations();
@@ -69,6 +71,25 @@ const PathsBasicNodeContent = (props) => {
       headers: getHttpHeaders({ instanceIdentifier: pathsInstance }),
     },
   });
+
+  useEffect(() => {
+    if (data) {
+      const nodeMetric = new DimensionalMetric(data.node.metricDim!);
+      const defaultConfig = nodeMetric.getDefaultSliceConfig(activeGoal);
+      const thisYear = nodeMetric.getSingleYear(
+        yearRange[1],
+        defaultConfig.categories
+      );
+
+      const yearTotal =
+        thisYear.rows[0] &&
+        thisYear.rows.reduce(
+          (partialSum, a) => (a ? partialSum + a[0] : partialSum),
+          0
+        );
+      onLoaded(categoryId, yearTotal || 0);
+    }
+  }, [activeGoal, data, yearRange]);
 
   if (loading) {
     return <ContentLoader type="grow" />;
@@ -105,6 +126,7 @@ const PathsBasicNodeContent = (props) => {
       )?.label;
 
       const unit = nodeMetric.getUnit();
+
       return (
         <CardContentBlock>
           <div>{nodeMetric.getName()}</div>
@@ -126,7 +148,7 @@ const PathsBasicNodeContent = (props) => {
 };
 
 const PathsActionNodeContent = (props) => {
-  const { node, pathsInstance } = props;
+  const { categoryId, node, pathsInstance, onLoaded } = props;
   const yearRange = useReactiveVar(yearRangeVar);
   const activeGoal = useReactiveVar(activeGoalVar);
   const t = useTranslations();
@@ -140,6 +162,14 @@ const PathsActionNodeContent = (props) => {
     },
   });
 
+  useEffect(() => {
+    if (data) {
+      const pathsAction = new PathsActionNode(data.action);
+      const impact = pathsAction.getYearlyImpact(yearRange[1]) || 0;
+      onLoaded(categoryId, impact);
+    }
+  }, [activeGoal, data, yearRange]);
+
   if (loading) {
     return <ContentLoader type="grow" />;
   }
@@ -148,15 +178,12 @@ const PathsActionNodeContent = (props) => {
   }
   if (data) {
     const pathsAction = new PathsActionNode(data.action);
+    const impact = pathsAction.getYearlyImpact(yearRange[1]) || 0;
     return (
       <CardContentBlock>
         {t('impact')} {yearRange[1]}
         <h4>
-          {yearRange ? (
-            beautifyValue(pathsAction.getYearlyImpact(yearRange[1]))
-          ) : (
-            <span>---</span>
-          )}
+          {yearRange ? beautifyValue(impact) : <span>---</span>}
           {pathsAction.getUnit()}
         </h4>
         <ActionParameters parameters={data.action.parameters} />
@@ -167,7 +194,7 @@ const PathsActionNodeContent = (props) => {
 };
 
 const PathsNodeContent = (props) => {
-  const { node, paths } = props;
+  const { categoryId, node, paths, onLoaded } = props;
 
   const { data, loading, error } = useQuery(GET_NODE_INFO, {
     fetchPolicy: 'no-cache',
@@ -186,16 +213,30 @@ const PathsNodeContent = (props) => {
   }
   if (data) {
     if (data.node.__typename === 'ActionNode') {
-      return <PathsActionNodeContent node={node} pathsInstance={paths} />;
+      return (
+        <PathsActionNodeContent
+          categoryId={categoryId}
+          node={node}
+          pathsInstance={paths}
+          onLoaded={onLoaded}
+        />
+      );
     } else if (data.node.__typename) {
-      return <PathsBasicNodeContent node={node} pathsInstance={paths} />;
+      return (
+        <PathsBasicNodeContent
+          categoryId={categoryId}
+          node={node}
+          pathsInstance={paths}
+          onLoaded={onLoaded}
+        />
+      );
     }
     return null;
   }
 };
 
 const CategoryCard = (props) => {
-  const { category, group, pathsInstance } = props;
+  const { category, group, pathsInstance, onLoaded } = props;
   return (
     <Card>
       {group && (
@@ -220,8 +261,10 @@ const CategoryCard = (props) => {
         )}
         {category.kausalPathsNodeUuid && (
           <PathsNodeContent
+            categoryId={category.id}
             node={category.kausalPathsNodeUuid}
             paths={pathsInstance}
+            onLoaded={onLoaded}
           />
         )}
       </div>
