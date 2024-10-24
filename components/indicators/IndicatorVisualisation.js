65b0e89590c12cfdb5c15850bb553c61b7d923a8
Lilia Sviridova <svirivoda@gmail.com>
2024-10-24 12:29:25 +0300

    Fix trend line not extending to the goal year

--- components/indicators/IndicatorVisualisation.js
+++ components/indicators/IndicatorVisualisation.js
@@ -310,8 +310,12 @@ const generateTrendTrace = (indicator, traces, goals, i18n) => {
 
     const highestDataYear = traces[0].y[traces[0].y.length - 1];
     const highestGoalYear = Math.max(
-      ...goals.map((goal) => goal.x[goal.x.length - 1])
+      ...goals.map((goal) => {
+        const goalYear = goal.x[goal.x.length - 1];
+        return goalYear ? parseInt(goalYear.split('-')[0], 10) : NaN;
+      })
     );
+
     if (highestGoalYear && highestGoalYear > highestDataYear) {
       predictedTrace.x.push(highestGoalYear);
     }
