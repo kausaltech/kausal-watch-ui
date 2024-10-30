3e2439d6f7849a0a04f50d098247a334d33ce02c
Tero Tikkanen <tero.tikkanen@gmail.com>
2024-10-30 16:44:40 +0200

    Fix tooltip

---
+++ components/common/Tooltip.tsx
@@ -0,0 +1,113 @@
+import React from 'react';
+
+import {
+  Button,
+  OverlayArrow,
+  Tooltip as AriaTooltip,
+  TooltipTrigger,
+} from 'react-aria-components';
+import styled from 'styled-components';
+
+const StyledTooltip = styled(AriaTooltip)`
+  box-shadow: 0 8px 20px rgba(0 0 0 / 0.1);
+  border-radius: 4px;
+  background: ${({ theme }) => theme.themeColors.black};
+  color: ${({ theme }) => theme.themeColors.white};
+  forced-color-adjust: none;
+  outline: none;
+  padding: 2px 8px;
+  max-width: 250px;
+  /* fixes FF gap */
+  transform: translate3d(0, 0, 0);
+
+  &[data-placement='top'] {
+    margin-bottom: 8px;
+    --origin: translateY(4px);
+  }
+
+  &[data-placement='bottom'] {
+    margin-top: 8px;
+    --origin: translateY(-4px);
+    & .react-aria-OverlayArrow svg {
+      transform: rotate(180deg);
+    }
+  }
+
+  &[data-placement='right'] {
+    margin-left: 8px;
+    --origin: translateX(-4px);
+    & .react-aria-OverlayArrow svg {
+      transform: rotate(90deg);
+    }
+  }
+
+  &[data-placement='left'] {
+    margin-right: 8px;
+    --origin: translateX(4px);
+    & .react-aria-OverlayArrow svg {
+      transform: rotate(-90deg);
+    }
+  }
+
+  & .react-aria-OverlayArrow svg {
+    display: block;
+    fill: var(--highlight-background);
+  }
+
+  &[data-entering] {
+    animation: slide 200ms;
+  }
+
+  &[data-exiting] {
+    animation: slide 200ms reverse ease-in;
+  }
+
+  @keyframes slide {
+    from {
+      transform: var(--origin);
+      opacity: 0;
+    }
+
+    to {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+`;
+
+const InfoButton = styled(Button)`
+  padding: 0 0.25rem 0.1rem;
+  line-height: 0.5rem;
+  opacity: 0.5;
+  svg {
+    fill: ${(props) =>
+      props.invert === 'true'
+        ? props.theme.themeColors.white
+        : props.theme.themeColors.black};
+  }
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+type TooltipProps = {
+  children: React.ReactNode;
+};
+const Tooltip = (props: TooltipProps) => {
+  const { children } = props;
+
+  return (
+    <StyledTooltip>
+      <OverlayArrow>
+        <svg width={8} height={8} viewBox="0 0 8 8">
+          <path d="M0 0 L4 4 L8 0" />
+        </svg>
+      </OverlayArrow>
+      {children}
+    </StyledTooltip>
+  );
+};
+
+export { TooltipTrigger };
+export default Tooltip;
