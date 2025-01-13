c2509a5c63f1cc9090b91065c349d6efb859ad2f
Lilia Sviridova <svirivoda@gmail.com>
2025-01-13 13:22:33 +0200

    Make date columns styling consistent

---
+++ components/dashboard/cells/DateCell.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import dayjs from 'common/dayjs';
+
+interface Props {
+  date: string | null;
+}
+
+const Wrapper = styled.div`
+  font-size: ${(props) => props.theme.fontSizeSm};
+  font-family: ${(props) =>
+    `${props.theme.fontFamilyTiny}, ${props.theme.fontFamilyFallback}`};
+  white-space: nowrap;
+  padding: ${(props) => props.theme.spaces.s050};
+  color: ${(props) => props.theme.textColorPrimary};
+`;
+
+const DateCell = ({ date }: Props) => {
+  const formattedDate = date ? dayjs(date).format('L') : null;
+  return <Wrapper>{formattedDate}</Wrapper>;
+};
+
+export default DateCell;
