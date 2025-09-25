kausaltech/kausal-watch-ui

commit 08ccd270060b21d679ed801878dd973dcc14514e

Fix type errors; support code coverage builds; update Sentry to v10

src/app/root/[domain]/[lang]/[plan]/(with-layout-elements)/[...slug]/ContentPage.tsx
@@ -5,19 +5,12 @@ import React from 'react';
 import { Col, Container, Row } from 'reactstrap';
 import { useTheme } from 'styled-components';
 
-import {
-  AccessibilityStatementPage,
-  ActionListPage,
-  CategoryPage,
-  CategoryTypePage,
-  EmptyPage,
-  ImpactGroupPage,
-  IndicatorListPage,
+import type {
+  GetContentPageQuery,
   MultiUseImageFragmentFragment,
-  PrivacyPolicyPage,
-  StaticPage,
 } from '@/common/__generated__/graphql';
 import { getBgImageAlignment } from '@/common/images';
+import { typenameMatches } from '@/common/utils';
 import CategoryPageContent from '@/components/categories/CategoryPageContent';
 import RichText from '@/components/common/RichText';
 import SecondaryNavigation from '@/components/common/SecondaryNavigation';
@@ -38,14 +31,57 @@ export type PageWithBody =
   | StaticPage
   | CategoryPage
   | CategoryTypePage;
-export type GeneralPlanPage = CategoryPage | StaticPage | EmptyPage | PageWithLeadContent;
+
+export type GeneralPlanPage = NonNullable<GetContentPageQuery['planPage']>;
+
+export type StaticPage = GeneralPlanPage & {
+  __typename: 'StaticPage';
+};
+
+export type EmptyPage = GeneralPlanPage & {
+  __typename: 'EmptyPage';
+};
+
+export type CategoryPage = GeneralPlanPage & {
+  __typename: 'CategoryPage';
+};
+
+export type AccessibilityStatementPage = GeneralPlanPage & {
+  __typename: 'AccessibilityStatementPage';
+};
+
+export type ActionListPage = GeneralPlanPage & {
+  __typename: 'ActionListPage';
+};
+
+export type ImpactGroupPage = GeneralPlanPage & {
+  __typename: 'ImpactGroupPage';
+};
+
+export type PrivacyPolicyPage = GeneralPlanPage & {
+  __typename: 'PrivacyPolicyPage';
+};
+
+export type CategoryTypePage = GeneralPlanPage & {
+  __typename: 'CategoryTypePage';
+};
+
+export type IndicatorListPage = GeneralPlanPage & {
+  __typename: 'IndicatorListPage';
+};
 
 type PageHeaderBlockProps = {
-  page: GeneralPlanPage;
+  page:
+    | AccessibilityStatementPage
+    | CategoryPage
+    | ActionListPage
+    | ImpactGroupPage
+    | IndicatorListPage
+    | StaticPage;
   color?: string | false | null | undefined;
 };
 
-const PageHeaderBlock = ({ color, page }: PageHeaderBlockProps) => {
+function PageHeaderBlock({ color, page }: PageHeaderBlockProps) {
   switch (page.__typename) {
     case 'CategoryPage': {
       const category = page.category;
@@ -54,8 +90,8 @@ const PageHeaderBlock = ({ color, page }: PageHeaderBlockProps) => {
         throw new Error('Category page without category configured');
       }
       const headerImage =
-        (category.image as MultiUseImageFragmentFragment | null) ||
-        (category.parent?.image as MultiUseImageFragmentFragment | null);
+        (category.image satisfies MultiUseImageFragmentFragment | null) ||
+        ((category.parent?.image ?? null) satisfies MultiUseImageFragmentFragment | null);
       const iconImage = category.iconImage?.rendition?.src;
 
       return (
@@ -93,43 +129,47 @@ const PageHeaderBlock = ({ color, page }: PageHeaderBlockProps) => {
       );
     }
   }
-};
+}
 
-export const Content = ({ page }: { page: GeneralPlanPage }) => {
+export default function ContentPage({ page }: { page: GeneralPlanPage }) {
   // TODO: Resolve shareImageUrl by pagetype
 
   const pathsInstance = usePaths();
   const theme = useTheme();
   const isCategoryPage = page.__typename === 'CategoryPage';
 
-  const isPageWithBody =
-    page.__typename === 'AccessibilityStatementPage' ||
-    page.__typename === 'StaticPage' ||
-    page.__typename === 'CategoryPage';
+  const isPageWithBody = typenameMatches(
+    page,
+    'AccessibilityStatementPage',
+    'StaticPage',
+    'CategoryPage'
+  );
 
-  const isStaticPage = page.__typename === 'StaticPage';
   const categoryColor = isCategoryPage && (page.category?.color || page.category?.parent?.color);
   const pageSectionColor = categoryColor || theme.themeColors.light;
 
-  const isParentWithSecondaryNav = (
-    parent: GeneralPlanPage['parent']
-  ): parent is StaticPage | EmptyPage =>
-    (parent as StaticPage | EmptyPage)?.__typename === 'StaticPage' ||
-    (parent as StaticPage | EmptyPage)?.__typename === 'EmptyPage';
-
-  const hasSecondaryNav = isParentWithSecondaryNav(page.parent)
-    ? (page.parent.childrenUseSecondaryNavigation ?? false)
-    : false;
-
-  const isPageWithLeadContent =
-    page.__typename === 'AccessibilityStatementPage' ||
-    page.__typename === 'ActionListPage' ||
-    page.__typename === 'ImpactGroupPage' ||
-    page.__typename === 'IndicatorListPage' ||
-    page.__typename === 'PrivacyPolicyPage';
+  const secondaryNavParent =
+    typenameMatches(page, 'StaticPage') &&
+    page.parent &&
+    (typenameMatches(page.parent, 'StaticPage') || typenameMatches(page.parent, 'EmptyPage')) &&
+    page.parent.childrenUseSecondaryNavigation
+      ? page.parent
+      : null;
+  const isPageWithLeadContent = typenameMatches(
+    page,
+    'AccessibilityStatementPage',
+    'ActionListPage',
+    'ImpactGroupPage',
+    'IndicatorListPage',
+    'PrivacyPolicyPage'
+  );
 
   // Restrict the secondary nav to be shown on StaticPages only currently
-  const siblings = hasSecondaryNav && isStaticPage ? (page?.parent?.children ?? []) : [];
+  const siblings = secondaryNavParent
+    ? typenameMatches(secondaryNavParent, 'StaticPage')
+      ? (secondaryNavParent.children ?? [])
+      : []
+    : [];
 
   if (pathsInstance)
     return (
@@ -137,43 +177,42 @@ export const Content = ({ page }: { page: GeneralPlanPage }) => {
         <PathsPageContent page={page} />
       </article>
     );
-  else
-    return (
-      <article>
+
+  console.log('render content page', page);
+
+  return (
+    <article>
+      {typenameMatches(page, 'CategoryPage', 'StaticPage') ? (
         <PageHeaderBlock page={page} color={isCategoryPage ? categoryColor : undefined} />
+      ) : null}
 
-        {isCategoryPage ? (
-          <CategoryPageContent page={page} pageSectionColor={pageSectionColor} />
-        ) : (
-          <div className="content-area">
-            {isPageWithLeadContent && page.leadContent && (
-              <Container className="my-5">
-                <Row>
-                  <Col lg={{ size: 8, offset: 2 }} md={{ size: 10, offset: 1 }}>
-                    <RichText html={page.leadContent} />
-                  </Col>
-                </Row>
-              </Container>
-            )}
+      {isCategoryPage ? (
+        <CategoryPageContent page={page} pageSectionColor={pageSectionColor} />
+      ) : (
+        <div className="content-area">
+          {isPageWithLeadContent && 'leadContent' in page && page.leadContent && (
+            <Container className="my-5">
+              <Row>
+                <Col lg={{ size: 8, offset: 2 }} md={{ size: 10, offset: 1 }}>
+                  <RichText html={page.leadContent} />
+                </Col>
+              </Row>
+            </Container>
+          )}
 
-            {siblings.length > 1 && (
-              <SecondaryNavigation
-                links={siblings}
-                activeLink={page.id}
-                title={page?.parent?.title || ''}
-              />
-            )}
+          {siblings.length > 1 && secondaryNavParent ? (
+            <SecondaryNavigation
+              links={siblings}
+              activeLink={page.id}
+              title={secondaryNavParent.title || ''}
+            />
+          ) : null}
 
-            {isPageWithBody && page.body && (
-              <StreamField
-                page={page}
-                blocks={page.body}
-                color={pageSectionColor}
-                hasSidebar={siblings.length > 1}
-              />
-            )}
-          </div>
-        )}
-      </article>
-    );
-};
+          {isPageWithBody && page.body && (
+            <StreamField page={page} blocks={page.body} hasSidebar={siblings.length > 1} />
+          )}
+        </div>
+      )}
+    </article>
+  );
+}

.reviewdog.yml
@@ -1,0 +1,7 @@
+runner:
+  eslint:
+    cmd: npm run lint:baseline --
+    format: eslint
+  typecheck:
+    cmd: npm run typecheck:baseline --
+    format: tsc

codegen.ts
@@ -18,6 +18,7 @@ const tsoConfig: TypeScriptDocumentsPluginConfig & TypeScriptPluginConfig = {
     PositiveInt: 'number',
     DateTime: 'string',
     JSONString: 'string',
+    Date: 'string',
   },
 };
 
@@ -46,7 +47,11 @@ const config: CodegenConfig = {
     'src/common/__generated__/graphql.ts': {
       schema: apolloConfig.client.service.url,
       documents: watchConfigDocs,
-      plugins: ['typescript', 'typescript-operations'],
+      plugins: [
+        { add: { content: '/* istanbul ignore file */' } },
+        'typescript',
+        'typescript-operations',
+      ],
       config: tsoConfig,
     },
     'src/common/__generated__/paths/possible_types.json': {

e2e-tests/__generated__/graphql.ts
@@ -12,12 +12,13 @@ export type Scalars = {
   Boolean: { input: boolean; output: boolean; }
   Int: { input: number; output: number; }
   Float: { input: number; output: number; }
-  Date: { input: any; output: any; }
+  Date: { input: string; output: string; }
   DateTime: { input: string; output: string; }
   JSONString: { input: string; output: string; }
   PointScalar: { input: any; output: any; }
   PositiveInt: { input: number; output: number; }
   UUID: { input: string; output: string; }
+  _Any: { input: any; output: any; }
 };
 
 /** An enumeration. */
@@ -201,6 +202,12 @@ export enum IndicatorTimeResolution {
   Year = 'YEAR'
 }
 
+export type InstanceContext = {
+  hostname: InputMaybe<Scalars['String']['input']>;
+  identifier: InputMaybe<Scalars['ID']['input']>;
+  locale: InputMaybe<Scalars['String']['input']>;
+};
+
 /** An enumeration. */
 export enum PlanFeaturesContactPersonsPublicData {
   /** Show all information */
@@ -343,13 +350,9 @@ export type UserFeedbackMutationInput = {
   url: Scalars['String']['input'];
 };
 
-/** An enumeration. */
 export enum WorkflowState {
-  /** Approved */
   Approved = 'APPROVED',
-  /** Draft */
   Draft = 'DRAFT',
-  /** Published */
   Published = 'PUBLISHED'
 }
 
@@ -419,9 +422,9 @@ export type PlaywrightGetPlanInfoQuery = (
         ) | null, children: Array<(
           { id: string, page: { __typename: 'AccessibilityStatementPage' | 'ActionListPage' | 'CategoryPage' | 'CategoryTypePage' | 'EmptyPage' | 'ImpactGroupPage' | 'IndicatorListPage' | 'Page' | 'PlanRootPage' | 'PrivacyPolicyPage' | 'StaticPage' } }
           & { __typename: 'PageMenuItem' }
-        ) | null> | null }
+        )> | null }
         & { __typename: 'PageMenuItem' }
-      ) | null> }
+      )> }
       & { __typename: 'MainMenu' }
     ) | null }
     & { __typename: 'Plan' }

e2e-tests/common/context.ts
@@ -283,6 +283,11 @@ export class PlanContext {
     return new PlanContext(data, baseURL, planIndicators);
   }
 
+  async waitForLoadingFinished(page: Page) {
+    await expect(page.locator('*[aria-busy=true]')).toHaveCount(0, { timeout: 30000 });
+    await page.waitForLoadState('networkidle');
+  }
+
   async checkAccessibility(page: Page) {
     await page.waitForLoadState('networkidle');
     const results = await new AxeBuilder({ page }).analyze();

e2e-tests/common/coverage.ts
@@ -1,0 +1,46 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { test as baseTest } from '@playwright/test';
+import crypto from 'crypto';
+import fs from 'fs';
+import path from 'path';
+
+const istanbulTempDir = process.env.ISTANBUL_TEMP_DIR
+  ? path.resolve(process.env.ISTANBUL_TEMP_DIR)
+  : path.join(process.cwd(), '.nyc_output');
+
+function generateUUID() {
+  return crypto.randomBytes(16).toString('hex');
+}
+
+const test = baseTest.extend({
+  context: async ({ context }, use) => {
+    await context.addInitScript({
+      content: `
+        window.addEventListener("beforeunload", () =>
+          window.collectIstanbulCoverage(JSON.stringify(window.__coverage__)),
+        )
+      `,
+    });
+    await fs.promises.mkdir(istanbulTempDir, { recursive: true });
+    await context.exposeFunction('collectIstanbulCoverage', (coverageJSON: string) => {
+      if (coverageJSON)
+        fs.writeFileSync(
+          path.join(istanbulTempDir, `playwright_coverage_${generateUUID()}.json`),
+          coverageJSON
+        );
+    });
+    await use(context);
+    for (const page of context.pages()) {
+      await page.evaluate(() =>
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
+        window.collectIstanbulCoverage(JSON.stringify(window.__coverage__))
+      );
+    }
+  },
+});
+
+const expect = test.expect;
+
+export { test, expect };

e2e-tests/playwright.config.ts
@@ -85,7 +85,7 @@ export default defineConfig({
   webServer: process.env.TEST_PAGE_BASE_URL
     ? undefined
     : {
-        command: process.env.TEST_DEVSERVER ? 'cd .. && pnpm dev' : 'cd .. && pnpm start',
+        command: process.env.TEST_DEVSERVER ? 'cd .. && pnpm dev' : 'cd .. && nyc pnpm start',
         url: 'http://localhost:3000/_health',
         reuseExistingServer: true,
         env: {

e2e-tests/tests/basic.spec.ts
@@ -1,9 +1,10 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { type Page, test as base, expect } from '@playwright/test';
+import { type Page, expect } from '@playwright/test';
 
 import { PlanContext, getIdentifiersToTest } from '../common/context.ts';
+import { test as coverageTest } from '../common/coverage.ts';
 
-const test = base.extend<{ ctx: PlanContext }>({});
+const test = coverageTest.extend<{ ctx: PlanContext }>({});
 
 async function navigateAndCheckLayout(page: Page, url: string, ctx: PlanContext) {
   await page.goto(url);
@@ -11,6 +12,7 @@ async function navigateAndCheckLayout(page: Page, url: string, ctx: PlanContext)
   await expect(page.locator('nav#branding-navigation-bar')).toBeVisible();
   await expect(page.locator('nav#global-navigation-bar')).toBeVisible();
   await expect(page.locator('main#main')).toBeVisible();
+  await expect(page.locator('*[aria-busy=true]')).toHaveCount(0, { timeout: 30000 });
   await page.waitForLoadState('networkidle');
   // temporarily skip accessibility check
   //await ctx.checkAccessibility(page);
@@ -198,6 +200,8 @@ const testPlan = (planId: string) =>
         });
         await firstIndicatorLink.waitFor();
         await firstIndicatorLink.click();
+        await page.waitForURL(/.*\/indicators\/[0-9]+/);
+        await ctx.waitForLoadingFinished(page);
         await expect(main).toBeVisible();
         const h1Span = page.locator('h1 >> span');
         await expect(h1Span).toContainText(indicatorName);

commit 48d98e9488c62483620b5d0935a29b68d8cda423

Fix bug which made secondary navigation disappear everywhere
Introduced in 08ccd270060b21d679ed801878dd973dcc14514e
The conditional was refactored to check for the wrong page's type

src/app/root/[domain]/[lang]/[plan]/(with-layout-elements)/[...slug]/ContentPage.tsx
@@ -166,7 +166,7 @@ export default function ContentPage({ page }: { page: GeneralPlanPage }) {
 
   // Restrict the secondary nav to be shown on StaticPages only currently
   const siblings = secondaryNavParent
-    ? typenameMatches(secondaryNavParent, 'StaticPage')
+    ? typenameMatches(page, 'StaticPage')
       ? (secondaryNavParent.children ?? [])
       : []
     : [];