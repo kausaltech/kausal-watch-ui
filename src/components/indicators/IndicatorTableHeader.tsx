a064d7ff004757adb43afd384a2dbbb084e50323
Tero Tikkanen <tero.tikkanen@gmail.com>
2025-09-30 19:09:55 +0300

    Align columns

--- src/components/indicators/IndicatorTableHeader.tsx
+++ src/components/indicators/IndicatorTableHeader.tsx
@@ -1,6 +1,14 @@
-const IndicatorTableHeader = (props: { column: string }) => {
+import type { IndicatorTableColumn } from './IndicatorListFilteredNew';
+
+const numericColumns: IndicatorTableColumn[] = ['latestValue', 'dimensions'];
+
+const IndicatorTableHeader = (props: { column: IndicatorTableColumn }) => {
   const { column } = props;
-  return <th key={column}>{column}</th>;
+  return (
+    <th key={column} style={{ textAlign: numericColumns.includes(column) ? 'right' : 'left' }}>
+      {column}
+    </th>
+  );
 };
 
 export default IndicatorTableHeader;
