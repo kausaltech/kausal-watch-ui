d492f404816f8d2182ee5e7104d4af18a64ffc8e
Joe Smallwood <joe.smallwood@kausal.tech>
2026-01-29 19:04:35 +0200

    Ensure the focal box of an image is always visible

--- src/components/contentblocks/FrontPageHeroBlock.tsx
+++ src/components/contentblocks/FrontPageHeroBlock.tsx
@@ -3,11 +3,21 @@ import React from 'react';
 import HeroFullImage from '@/components/home/HeroFullImage';
 import HeroSmallImage from '@/components/home/HeroSmallImage';
 
+interface FocalBoxInfo {
+  focalPointX: number;
+  focalPointY: number;
+  focalPointWidth: number;
+  focalPointHeight: number;
+  imageWidth: number;
+  imageHeight: number;
+}
+
 interface FrontPageHeroProps {
   id: string;
   layout: 'small_image' | 'big_image';
   image: any;
   imageAlign: string;
+  focalBox?: FocalBoxInfo;
   heading: string | null | undefined;
   lead: string | null | undefined;
   altText: string | null | undefined;
@@ -20,6 +30,7 @@ const FrontPageHeroBlock = (props: FrontPageHeroProps) => {
     layout = 'big_image',
     image,
     imageAlign,
+    focalBox,
     heading,
     lead,
     altText,
@@ -48,10 +59,11 @@ const FrontPageHeroBlock = (props: FrontPageHeroProps) => {
       id={id}
       bgImage={imageSrc}
       imageAlign={imageAlign}
-      title={heading}
-      lead={lead}
-      imageCredit={imageCredit}
-      altText={altText}
+      focalBox={focalBox}
+      title={heading ?? undefined}
+      lead={lead ?? undefined}
+      imageCredit={imageCredit ?? undefined}
+      altText={altText ?? undefined}
     />
   );
 };
