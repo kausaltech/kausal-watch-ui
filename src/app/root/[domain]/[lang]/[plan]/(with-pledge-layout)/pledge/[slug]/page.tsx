9aecf24cc20df4b6341c80f22d491fbaef0a5962
Joe Smallwood <joe.smallwood@kausal.tech>
2026-02-03 11:45:56 +0200

    Switch to client components

--- src/app/root/[domain]/[lang]/[plan]/(with-pledge-layout)/pledge/[slug]/page.tsx
+++ src/app/root/[domain]/[lang]/[plan]/(with-pledge-layout)/pledge/[slug]/page.tsx
@@ -1,32 +1,19 @@
-import type { Metadata, ResolvingMetadata } from 'next';
-import { getTranslations } from 'next-intl/server';
+'use client';
+
+import { useTranslations } from 'next-intl';
 import { Container } from 'reactstrap';
 
 type Props = {
   params: Promise<{ plan: string; lang: string; slug: string }>;
 };
 
-export async function generateMetadata(props: Props, parent: ResolvingMetadata): Promise<Metadata> {
-  const params = await props.params;
-  const t = await getTranslations();
-  const parentMetadata = await parent;
-
-  return {
-    title: `${t('pledge-details-title')} | ${parentMetadata.title?.absolute}`,
-  };
-}
-
-export default async function PledgeDetailsPage(props: Props) {
-  const params = await props.params;
-  const t = await getTranslations();
-  const { slug } = params;
+export default async function PledgeDetailsPage({ params }) {
+  const t = useTranslations();
 
   return (
     <Container className="py-5">
       <h1>{t('pledge-details-title')}</h1>
-      <p>
-        {t('pledge-details-slug')}: {slug}
-      </p>
+      <p>{params.slug}</p>
     </Container>
   );
 }
