a0816f1c6152a03ddff2c4436805ace1f1b3cc17
Joe Smallwood <joe.smallwood@kausal.tech>
2026-02-12 14:13:21 +0200

    Add empty pledge list styles and content

--- src/components/pledge/PledgeList.tsx
+++ src/components/pledge/PledgeList.tsx
@@ -7,6 +7,7 @@ import { Button, ButtonGroup, Container } from 'reactstrap';
 import styled from 'styled-components';
 
 import type { GetPledgesQuery } from '@/common/__generated__/graphql';
+import Icon from '@/components/common/Icon';
 import { getDefaultFormFields } from '@/utils/pledge.utils';
 
 import ConfirmPledge from './ConfirmPledge';
@@ -19,9 +20,17 @@ type Props = {
   pledges: Pledge[];
 };
 
+/**
+ * Wrapper to allow child sections to stretch outside of the parent containers gutters
+ * with ::before pseudo elements.
+ */
+const StyledPageWrapper = styled.div`
+  overflow-x: hidden;
+  position: relative;
+`;
+
 const StyledContainer = styled(Container)`
   margin-top: ${({ theme }) => theme.spaces.s200};
-  margin-bottom: ${({ theme }) => theme.spaces.s200};
 `;
 
 const StyledRadioButton = styled(Button)`
@@ -44,6 +53,67 @@ const StyledPledgeGrid = styled.div`
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
   gap: ${({ theme }) => theme.spaces.s200};
   align-items: stretch;
+  margin-bottom: ${({ theme }) => theme.spaces.s200};
+`;
+
+const StyledEmptyState = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  padding: ${({ theme }) => theme.spaces.s300} ${({ theme }) => theme.spaces.s200};
+  background: ${({ theme }) => theme.graphColors.grey010};
+  position: relative;
+
+  // Stretch the background outside of the parent container gutters... it looks better
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    background: ${({ theme }) => theme.graphColors.grey010};
+    width: 100vw;
+    z-index: -1;
+  }
+`;
+
+const StyledEmptyIconWrapper = styled.div`
+  margin-bottom: ${({ theme }) => theme.spaces.s200};
+  color: ${({ theme }) => theme.brandDark};
+`;
+
+const StyledEmptyHeading = styled.h2`
+  font-size: ${({ theme }) => theme.fontSizeMd};
+  font-weight: ${({ theme }) => theme.fontWeightBold};
+  margin-bottom: ${({ theme }) => theme.spaces.s100};
+  max-width: 500px;
+`;
+
+const StyledEmptySubtext = styled.p`
+  font-size: ${({ theme }) => theme.fontSizeBase};
+  color: ${({ theme }) => theme.textColor.secondary};
+  margin-bottom: ${({ theme }) => theme.spaces.s200};
+`;
+
+const StyledEmptyCardWrapper = styled.div`
+  width: 100%;
+  max-width: 480px;
+  text-align: left;
+  margin-bottom: ${({ theme }) => theme.spaces.s200};
+`;
+
+const StyledBrowseAllButton = styled.button`
+  background: none;
+  border: none;
+  padding: 0;
+  font-size: ${({ theme }) => theme.fontSizeBase};
+  color: ${({ theme }) => theme.linkColor};
+  cursor: pointer;
+  text-decoration: underline;
+
+  &:hover {
+    color: ${({ theme }) => theme.linkColor};
+  }
 `;
 
 type ViewType = 'ALL' | 'MY_PLEDGES';
@@ -109,73 +179,102 @@ function PledgeList({ pledges }: Props) {
   }
 
   return (
-    <StyledContainer>
-      <StyledTabsContainer>
-        <ButtonGroup
-          size="small"
-          role="radiogroup"
-          aria-label={t('pledge-list-view-toggle')}
-          onKeyDown={handleRadiosKeyDown}
-        >
-          <StyledRadioButton
-            color="black"
+    <StyledPageWrapper>
+      <StyledContainer>
+        <StyledTabsContainer>
+          <ButtonGroup
             size="small"
-            outline
-            onClick={() => setView('ALL')}
-            active={view === 'ALL'}
-            aria-checked={view === 'ALL'}
-            role="radio"
-            tabIndex={view === 'ALL' ? 0 : -1}
+            role="radiogroup"
+            aria-label={t('pledge-list-view-toggle')}
+            onKeyDown={handleRadiosKeyDown}
           >
-            {t('pledge-list-all')}
-          </StyledRadioButton>
-          <StyledRadioButton
-            color="black"
-            size="small"
-            outline
-            onClick={() => setView('MY_PLEDGES')}
-            active={view === 'MY_PLEDGES'}
-            aria-checked={view === 'MY_PLEDGES'}
-            role="radio"
-            tabIndex={view === 'MY_PLEDGES' ? 0 : -1}
-          >
-            {t('pledge-list-mine')}
-          </StyledRadioButton>
-        </ButtonGroup>
-      </StyledTabsContainer>
+            <StyledRadioButton
+              color="black"
+              size="small"
+              outline
+              onClick={() => setView('ALL')}
+              active={view === 'ALL'}
+              aria-checked={view === 'ALL'}
+              role="radio"
+              tabIndex={view === 'ALL' ? 0 : -1}
+            >
+              {t('pledge-list-all')}
+            </StyledRadioButton>
+            <StyledRadioButton
+              color="black"
+              size="small"
+              outline
+              onClick={() => setView('MY_PLEDGES')}
+              active={view === 'MY_PLEDGES'}
+              aria-checked={view === 'MY_PLEDGES'}
+              role="radio"
+              tabIndex={view === 'MY_PLEDGES' ? 0 : -1}
+            >
+              {t('pledge-list-mine')}
+            </StyledRadioButton>
+          </ButtonGroup>
+        </StyledTabsContainer>
 
-      <StyledPledgeGrid>
-        {filteredPledges.map((pledge) => (
-          <PledgeCard
-            key={pledge.slug}
-            title={pledge.name}
-            description={pledge.description}
-            slug={pledge.slug}
-            image={pledge.image?.large?.src ?? pledge.image?.full?.src}
-            imageAlt={pledge.image?.altText ?? pledge.name}
-            isCommitted={committedSlugs.has(pledge.slug)}
-            committedCount={pledge.commitmentCount + getCommitmentCountAdjustment(pledge.slug)}
-            onCommitClick={(isCommitted) => handleCommitClick(pledge, isCommitted)}
+        {filteredPledges.length > 0 && (
+          <StyledPledgeGrid>
+            {filteredPledges.map((pledge) => (
+              <PledgeCard
+                key={pledge.slug}
+                title={pledge.name}
+                description={pledge.description}
+                slug={pledge.slug}
+                image={pledge.image?.large?.src ?? pledge.image?.full?.src}
+                imageAlt={pledge.image?.altText ?? pledge.name}
+                isCommitted={committedSlugs.has(pledge.slug)}
+                committedCount={pledge.commitmentCount + getCommitmentCountAdjustment(pledge.slug)}
+                onCommitClick={(isCommitted) => handleCommitClick(pledge, isCommitted)}
+              />
+            ))}
+          </StyledPledgeGrid>
+        )}
+
+        {view === 'MY_PLEDGES' && filteredPledges.length === 0 && pledges.length > 0 && (
+          <StyledEmptyState>
+            <StyledEmptyIconWrapper>
+              <Icon name="globe" width="48px" height="48px" />
+            </StyledEmptyIconWrapper>
+            <StyledEmptyHeading>{t('pledge-empty-heading')}</StyledEmptyHeading>
+            <StyledEmptySubtext>{t('pledge-empty-subtext')}</StyledEmptySubtext>
+            <StyledEmptyCardWrapper>
+              <PledgeCard
+                layout="mini"
+                title={pledges[0].name}
+                slug={pledges[0].slug}
+                image={pledges[0].image?.large?.src ?? pledges[0].image?.full?.src}
+                imageAlt={pledges[0].image?.altText ?? pledges[0].name}
+                committedCount={
+                  pledges[0].commitmentCount + getCommitmentCountAdjustment(pledges[0].slug)
+                }
+                isCommitted={false}
+                onCommitClick={() => handleCommitClick(pledges[0], false)}
+              />
+            </StyledEmptyCardWrapper>
+            <StyledBrowseAllButton onClick={() => setView('ALL')}>
+              {t('pledge-empty-browse-all')}
+            </StyledBrowseAllButton>
+          </StyledEmptyState>
+        )}
+
+        {selectedPledge && (
+          <ConfirmPledge
+            isOpen={showConfirmDrawer}
+            onClose={handleCloseDrawer}
+            onConfirm={handleConfirmPledge}
+            pledgeName={selectedPledge.name}
+            pledgeSlug={selectedPledge.slug}
+            pledgeImage={selectedPledge.image?.rendition?.src ?? null}
+            commitmentCount={selectedPledge.commitmentCount}
+            formFields={getDefaultFormFields(t)}
+            userData={userData}
           />
-        ))}
-      </StyledPledgeGrid>
-
-      {view === 'MY_PLEDGES' && filteredPledges.length === 0 && <p>{t('pledge-no-commitments')}</p>}
-
-      {selectedPledge && (
-        <ConfirmPledge
-          isOpen={showConfirmDrawer}
-          onClose={handleCloseDrawer}
-          onConfirm={handleConfirmPledge}
-          pledgeName={selectedPledge.name}
-          pledgeSlug={selectedPledge.slug}
-          pledgeImage={selectedPledge.image?.rendition?.src ?? null}
-          commitmentCount={selectedPledge.commitmentCount}
-          formFields={getDefaultFormFields(t)}
-          userData={userData}
-        />
-      )}
-    </StyledContainer>
+        )}
+      </StyledContainer>
+    </StyledPageWrapper>
   );
 }
 
