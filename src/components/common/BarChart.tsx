71d32dfcc3d7f8e7ab79cae2bcc16fe4def6d0c4
Liliia Sviridova <lilia.sviridova@kausal.tech>
2026-02-23 13:21:59 +0200

    Align action phases labels with bar segments

--- src/components/common/BarChart.tsx
+++ src/components/common/BarChart.tsx
@@ -19,37 +19,37 @@ const Status = styled.div`
 const BarGraph = styled.div`
   display: flex;
   height: 1rem;
-  width: auto;
+  width: 100%;
   background-color: ${(props) => props.theme.themeColors.light};
 `;
 
-const Segment = styled.div<{ portion: number }>`
+const Segment = styled.div<{ portion: number; color: string }>`
   background-color: ${(props) => props.color};
   width: ${(props) => props.portion}%;
   height: 1rem;
 `;
 
-const Labels = styled.div`
-  display: flex;
-  width: auto;
+const Labels = styled.div<{ columns: string }>`
+  display: grid;
+  grid-template-columns: ${(props) => props.columns};
+  width: 100%;
+  margin-top: ${(props) => props.theme.spaces.s050};
+  column-gap: 0;
 `;
 
 const SegmentLabel = styled.span<{ portion: number }>`
   display: flex;
   flex-direction: column;
-  flex-basis: ${(props) => props.portion}%;
-  text-align: left;
-  margin: ${(props) => props.theme.spaces.s050} ${(props) => props.theme.spaces.s050} 0 0;
+  align-items: center;
+  text-align: center;
+  padding: 0 ${(props) => props.theme.spaces.s050};
   font-size: ${(props) => props.theme.fontSizeSm};
   font-family: ${(props) => `${props.theme.fontFamilyTiny}, ${props.theme.fontFamilyFallback}`};
   line-height: ${(props) => props.theme.lineHeightMd};
 
-  span {
-    align-self: flex-start;
-  }
-
   .value {
     font-weight: ${(props) => props.theme.fontWeightBold};
+    white-space: nowrap;
   }
 `;
 
@@ -82,6 +82,10 @@ function BarChart({ title, data }: BarChartProps) {
     }))
     .filter(({ portion }) => portion > 0);
 
+  // Match grid columns with bar segment width
+  const labelColumns =
+    segments.length === 1 ? '1fr' : segments.map((s) => `${s.portion * 100}%`).join(' ');
+
   return (
     <BarChartWrapper>
       <h3>{title}</h3>
@@ -92,12 +96,9 @@ function BarChart({ title, data }: BarChartProps) {
               <Segment key={segment.id} color={segment.color} portion={segment.portion * 100} />
             ))}
           </BarGraph>
-          <Labels>
+          <Labels columns={labelColumns}>
             {segments.map((segment) => (
-              <SegmentLabel
-                key={segment.id}
-                portion={(segments.length === 1 ? 1 : segment.portion) * 100}
-              >
+              <SegmentLabel key={segment.id}>
                 <span className="value">{segment.value}</span>
                 <span> {segment.label}</span>
               </SegmentLabel>
