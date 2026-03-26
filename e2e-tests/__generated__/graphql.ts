bc3bde721ed6dd6aed4d4b9b3851da7dfe8ee5d6
Joe Smallwood <joe.smallwood@kausal.tech>
2026-03-26 14:51:50 +0200

    Add query and boilerplate for indicator factor visualisations

--- e2e-tests/__generated__/graphql.ts
+++ e2e-tests/__generated__/graphql.ts
@@ -460,7 +460,7 @@ export enum PlanFeaturesContactPersonsPublicData {
   None = 'NONE'
 }
 
-/** PlanFeatures(id, latest_revision, plan, allow_images_for_actions, show_admin_link, allow_public_site_login, expose_unpublished_plan_only_to_authenticated_user, contact_persons_public_data, contact_persons_show_picture, contact_persons_show_organization_ancestors, contact_persons_hide_moderators, has_action_identifiers, show_action_identifiers, has_action_contact_person_roles, minimal_statuses, has_action_official_name, has_action_lead_paragraph, has_action_primary_orgs, enable_search, enable_indicator_comparison, enable_indicator_factors, indicator_ordering, moderation_workflow, display_field_visibility_restrictions, output_report_action_print_layout, password_protected, indicators_open_in_modal, enable_change_log, enable_community_engagement, enable_action_pdf_export_in_public_ui, admin_accessibility_conformance_level) */
+/** PlanFeatures(id, latest_revision, plan, allow_images_for_actions, show_admin_link, allow_public_site_login, expose_unpublished_plan_only_to_authenticated_user, contact_persons_public_data, contact_persons_show_picture, contact_persons_show_organization_ancestors, contact_persons_hide_moderators, has_action_identifiers, show_action_identifiers, has_action_contact_person_roles, minimal_statuses, has_action_official_name, has_action_lead_paragraph, has_action_primary_orgs, enable_search, enable_indicator_comparison, enable_indicator_factors, indicator_ordering, moderation_workflow, display_field_visibility_restrictions, output_report_action_print_layout, password_protected, indicators_open_in_modal, enable_change_log, enable_community_engagement, admin_accessibility_conformance_level) */
 export type PlanFeaturesInput = {
   /** Set if the plan uses meaningful action identifiers */
   hasActionIdentifiers: InputMaybe<Scalars['Boolean']['input']>;
@@ -481,7 +481,7 @@ export type PlanInput = {
   /** ISO 3166-1 country code (e.g. FI, DE, US) */
   country: Scalars['String']['input'];
   features: InputMaybe<PlanFeaturesInput>;
-  /** A unique identifier for the plan used internally to distinguish between plans. This becomes part of the site URL used during onboarding. Use lowercase letters and dashes. */
+  /** A unique identifier for the plan used internally to distinguish between plans. This becomes part of the test site URL: https://[identifier].watch-test.kausal.tech. Use lowercase letters and dashes. */
   identifier: Scalars['ID']['input'];
   /** The official plan name in full form */
   name: Scalars['String']['input'];
