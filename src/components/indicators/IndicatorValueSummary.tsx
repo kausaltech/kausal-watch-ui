2bcd0c9c528679c101e17df4c986406e28ad5012
Liliia Sviridova <lilia.sviridova@kausal.tech>
2026-04-01 13:06:35 +0300

    Reuse shared summary styles

--- src/components/indicators/IndicatorValueSummary.tsx
+++ src/components/indicators/IndicatorValueSummary.tsx
@@ -14,59 +14,16 @@ import Icon from '@/components/common/Icon';
 import PopoverTip from '@/components/common/PopoverTip';
 
 import dayjs from '../../common/dayjs';
+import {
+  ValueBlock,
+  ValueDate,
+  ValueDisplay,
+  ValueLabel,
+  ValueSummary,
+  ValueUnit,
+} from './IndicatorSummary.styles';
 
 const DEFAULT_ROUNDING = 2;
-
-const ValueSummary = styled.div`
-  display: flex;
-  gap: ${(props) => props.theme.spaces.s100};
-  flex-direction: row;
-  align-items: flex-start;
-  justify-content: stretch;
-  text-align: left;
-  margin-bottom: ${(props) => props.theme.spaces.s100};
-  padding-top: ${(props) => props.theme.spaces.s100};
-  border-top: 1px solid ${(props) => props.theme.graphColors.grey030};
-  border-bottom: 1px solid ${(props) => props.theme.graphColors.grey030};
-`;
-
-const ValueBlock = styled.div`
-  flex: 1 0 auto;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  text-align: left;
-`;
-
-const ValueLabel = styled.div`
-  margin-bottom: ${(props) => props.theme.spaces.s050};
-  font-size: ${(props) => props.theme.fontSizeBase};
-  font-weight: ${(props) => props.theme.fontWeightBold};
-  line-height: ${(props) => props.theme.lineHeightSm};
-`;
-
-const ValueDate = styled.div`
-  font-size: ${(props) => props.theme.fontSizeSm};
-  color: ${(props) => props.theme.themeColors.dark};
-`;
-
-const ValueDisplay = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  text-align: left;
-  font-size: ${(props) => props.theme.fontSizeLg};
-  font-weight: ${(props) => props.theme.fontWeightBold};
-  line-height: ${(props) => props.theme.lineHeightSm};
-  margin-bottom: ${(props) => props.theme.spaces.s100};
-`;
-
-const ValueUnit = styled.span`
-  margin: 0 0.5em 0 0.25em;
-  font-size: ${(props) => props.theme.fontSizeBase};
-  font-weight: ${(props) => props.theme.fontWeightNormal};
-  color: ${(props) => props.theme.themeColors.dark};
-`;
 
 const ValueChange = styled.div`
   margin-top: ${(props) => props.theme.spaces.s050};
