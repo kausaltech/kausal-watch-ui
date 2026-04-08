2ef3fc3a82de39db14a15f47d39e908792bb4457
Tero Tikkanen <tero.tikkanen@gmail.com>
2026-04-08 14:02:14 +0300

    Type IndicatorShowcaseBlock

--- src/components/contentblocks/IndicatorShowcaseBlock.tsx
+++ src/components/contentblocks/IndicatorShowcaseBlock.tsx
@@ -3,13 +3,28 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { Paper } from '@mui/material';
 import { useTranslations } from 'next-intl';
-import PropTypes from 'prop-types';
 import { Alert, Col, Container, Row } from 'reactstrap';
 
+import type { StreamFieldFragmentFragment } from '@/common/__generated__/graphql';
 import { deploymentType } from '@/common/environment';
 import RichText from '@/components/common/RichText';
 import IndicatorProgressBar from '@/components/indicators/IndicatorProgressBar';
 import IndicatorVisualisation from '@/components/indicators/IndicatorVisualisation';
+
+type IndicatorShowcaseBlockData = Extract<
+  StreamFieldFragmentFragment,
+  { __typename: 'IndicatorShowcaseBlock' }
+>;
+type Indicator = NonNullable<IndicatorShowcaseBlockData['indicator']>;
+
+interface IndicatorShowcaseBlockProps {
+  id?: string;
+  title: IndicatorShowcaseBlockData['title'];
+  body: IndicatorShowcaseBlockData['body'];
+  significantDigits: IndicatorShowcaseBlockData['significantDigits'];
+  indicator: Indicator;
+  linkButton: IndicatorShowcaseBlockData['linkButton'];
+}
 
 const IndicatorShowcase = styled.div`
   padding: ${(props) => props.theme.spaces.s400} 0;
@@ -56,15 +71,21 @@ const getNormalizedUnit = (indicator) => {
   }
 };
 
-const IndicatorShowcaseBlock = (props) => {
+const IndicatorShowcaseBlock = (props: IndicatorShowcaseBlockProps) => {
   const { id = '', indicator, title, body, significantDigits, linkButton } = props;
 
   const t = useTranslations();
-  const lastGoal = indicator.goals[indicator.goals.length - 1];
+  const goals = indicator.goals ?? [];
+  const lastGoal = goals[goals.length - 1];
   const firstValue = indicator.values[0];
   const lastValue = indicator.values[indicator.values.length - 1];
 
-  const indicatorHasGoal = indicator.goals.length > 0;
+  const indicatorHasGoal = goals.length > 0;
+
+  const pageLinkButton =
+    linkButton?.__typename === 'PageLinkBlock'
+      ? { text: linkButton.text ?? undefined, page: linkButton.page }
+      : null;
 
   let indicatorVisualisation: React.ReactNode | null = null;
   if (!indicatorHasGoal) {
@@ -81,28 +102,27 @@ const IndicatorShowcaseBlock = (props) => {
       </>
     );
   } else {
-    console.log('indicator', indicator, indicatorHasGoal);
     // Only show normalization if all values including goal are available as normalized
     const canNormalize =
-      getNormalizedValue(firstValue, indicator) &&
-      getNormalizedValue(lastValue, indicator) &&
-      getNormalizedValue(lastGoal, indicator);
+      !!getNormalizedValue(firstValue, indicator) &&
+      !!getNormalizedValue(lastValue, indicator) &&
+      !!getNormalizedValue(lastGoal, indicator);
 
     const baseValue = {
-      date: firstValue.date,
+      date: firstValue.date ?? '',
       value: firstValue.value,
       normalizedValue: getNormalizedValue(firstValue, indicator),
     };
 
     const latestValue = {
-      date: lastValue.date,
+      date: lastValue.date ?? '',
       value: lastValue.value,
       normalizedValue: getNormalizedValue(lastValue, indicator),
     };
 
     const goalValue = {
-      date: lastGoal.date,
-      value: lastGoal.value,
+      date: lastGoal?.date ?? '',
+      value: lastGoal?.value ?? 0,
       normalizedValue: getNormalizedValue(lastGoal, indicator),
     };
 
@@ -114,14 +134,13 @@ const IndicatorShowcaseBlock = (props) => {
       <IndicatorProgressBar
         indicatorId={indicator.id}
         note={indicator.name}
-        linkButton={linkButton}
-        significantDigits={significantDigits}
+        linkButton={pageLinkButton}
+        significantDigits={significantDigits ?? undefined}
         baseValue={baseValue}
         lastValue={latestValue}
         goalValue={goalValue}
         normalize={canNormalize}
         unit={unit}
-        indicator={indicator}
       />
     );
   }
@@ -131,31 +150,13 @@ const IndicatorShowcaseBlock = (props) => {
         <Row>
           <Col xl={{ size: 8, offset: 2 }} lg={{ size: 10, offset: 1 }}>
             <h2>{title}</h2>
-            <RichText html={body} className="mb-5" />
+            <RichText html={body ?? ''} className="mb-5" />
             {indicatorVisualisation}
           </Col>
         </Row>
       </Container>
     </IndicatorShowcase>
   );
-};
-
-/*
-  startDate: PropTypes.string,
-  startValue: PropTypes.string.isRequired,
-  latestDate: PropTypes.string,
-  latestValue: PropTypes.string.isRequired,
-  goalDate: PropTypes.string,
-  goalValue: PropTypes.string.isRequired,
-  unit: PropTypes.string.isRequired,
-  note: PropTypes.string,
-  */
-
-IndicatorShowcaseBlock.propTypes = {
-  id: PropTypes.string,
-  indicator: PropTypes.shape({
-    id: PropTypes.string,
-  }).isRequired,
 };
 
 export default IndicatorShowcaseBlock;
