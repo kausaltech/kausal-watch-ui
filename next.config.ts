20f2c302f0eeb48fd0f4f01ad1cbb715342c7860
Juha Yrjölä <juha.yrjola@kausal.tech>
2026-07-30 12:43:26 +0300

    configure allowedDevOrigins for local dev

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,8 @@ import type * as Webpack from 'webpack';
 
 import { getNextConfig } from './kausal_common/configs/common-next-config.ts';
 import { wrapWithSentryConfig } from './kausal_common/configs/sentry-next-config.ts';
+import { getWildcardDomains } from './kausal_common/src/env/runtime.ts';
+import { isLocalDev } from './kausal_common/src/env/static.ts';
 import { initializeThemes } from './kausal_common/src/themes/next-config.mjs';
 
 process.env.NEXT_TELEMETRY_DISABLED = '1';
@@ -59,6 +61,10 @@ let nextConfig: NextConfig = {
     ],
   },
 };
+
+if (isLocalDev) {
+  nextConfig.allowedDevOrigins = getWildcardDomains().map((domain) => `*.${domain}`);
+}
 
 nextConfig = wrapWithSentryConfig(nextConfig);
 nextConfig = withNextIntl('./src/config/i18n.ts')(nextConfig);
