ddf45c6adb2777a533250e3640507eb80c197139
Tero Tikkanen <tero.tikkanen@gmail.com>
2026-08-17 12:54:53 +0300

    Use actionListBlock background color from themes

--- src/tests/test-utils.js
+++ src/tests/test-utils.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-import { ThemeProvider } from '@emotion/react';
 import { ThemeProvider as MUIThemeProvider } from '@mui/material/styles';
+
+import { ThemeProvider } from '@emotion/react';
+
 import { render } from '@testing-library/react';
 import { NextIntlClientProvider } from 'next-intl';
 
@@ -10,7 +12,15 @@ import { initializeMuiTheme } from '@common/themes/mui-theme/theme';
 import a11yMessages from '../../locales/en/a11y.json';
 import actionsMessages from '../../locales/en/actions.json';
 import commonMessages from '../../locales/en/common.json';
-import theme from '../../public/static/themes/default/theme.json';
+import themeJson from '../../public/static/themes/default/theme.json';
+
+/*
+ * Importing JSON widens literal values to plain strings, which no longer
+ * satisfies Theme's literal-union properties (e.g. headingsTextTransform).
+ */
+const theme = /** @type {import('@kausal/themes/types').Theme} */ (
+  /** @type {unknown} */ (themeJson)
+);
 
 const messages = { ...a11yMessages, ...actionsMessages, ...commonMessages };
 const muiTheme = initializeMuiTheme(theme);
