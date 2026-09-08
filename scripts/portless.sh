c1bf4422a79dfc672256ad01a7559d228f770098
Tero Tikkanen <tero.tikkanen@gmail.com>
2026-09-08 13:46:16 +0300

    Remove --webpack from portless spell

--- scripts/portless.sh
+++ scripts/portless.sh
@@ -6,4 +6,4 @@ function stitle() {
 }
 stitle "portless: kausal-watch-ui"
 # Portless is used for theme development and --webpack is needed to preview themes locally
-portless --name watch-ui --app-port 8052 pnpm dev --webpack
+portless --name watch-ui --app-port 8052 pnpm dev
