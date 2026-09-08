bf20b1746e9eaa4994321b314adef81521be3017
Tero Tikkanen <tero.tikkanen@gmail.com>
2026-09-08 15:23:51 +0300

    Remove hidesearch prop

--- src/components/common/GlobalNav.tsx
+++ src/components/common/GlobalNav.tsx
@@ -577,7 +577,6 @@ export type GlobalNavProps = {
   externalItems?: ExternalItem[];
   fullwidth?: boolean;
   hidePlanSelector?: boolean;
-  hideSearch?: boolean;
   hideVersionSelector?: boolean;
   logoLink?: string | null;
   navItems: NavItems;
@@ -603,7 +602,6 @@ function GlobalNav(props: GlobalNavProps) {
     customToolbarItems,
     logoLink: logoLinkOverride,
     hidePlanSelector = false,
-    hideSearch = false,
     hideVersionSelector = false,
   } = props;
   const rootLink = logoLinkOverride ?? defaultRootLink;
@@ -678,7 +676,7 @@ function GlobalNav(props: GlobalNavProps) {
 
           <Nav navbar className="ml-auto d-none d-md-flex">
             {customToolbarItems.length > 0 && <CustomToolbar items={customToolbarItems} />}
-            {!hideSearch && <NavbarSearch />}
+            {plan.features.enableSearch && <NavbarSearch />}
             <LanguageSelector mobile={false} />
           </Nav>
           <NavbarToggler
@@ -748,7 +746,7 @@ function GlobalNav(props: GlobalNavProps) {
                     </NavItem>
                   )
                 )}
-              {!hideSearch && plan.features.enableSearch && (
+              {plan.features.enableSearch && (
                 <NavItem className="d-md-none mb-2">
                   <NavLink>
                     <NavigationLink slug="/search" onClick={handleClose}>
